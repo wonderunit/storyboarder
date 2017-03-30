e0c52290c930d687bf061b71d819f2f0b4afdc75
Eric Skogen <audionerd@users.noreply.github.com>
2017-03-30 13:35:38 -0500

    add speed, total distance, total time

--- src/js/window/sonifier.js
+++ src/js/window/sonifier.js
@@ -1,35 +1,72 @@
+/*
+    x
+    y
+
+    pressure
+    tilt
+    angle
+
+    eraser
+    pointerType
+
+    sW
+    sH
+
+    timestamp
+*/
 const loop = require('raf-loop')
 
 const distance = (x1, y1, x2, y2) =>
   Math.hypot(x2 - x1, y2 - y1)
 
+const createModel = () => ({
+  speed: 0,
+
+  totalDistance: 0,
+  totalTime: 0
+})
+
 let engine
+let stack
+let model
 
-let stack = []
-
-let model = {
-  speed: 0
+const init = () => {
+  engine = loop(step)
+  stack = []
 }
 
-const init = () => engine = loop(step)
+const start = () => {
+  engine.start()
+  stack = []
+  model = createModel()
+}
 
-const start = () => engine.start()
-
-const stop = () => engine.stop()
+const stop = () => {
+  engine.stop()
+}
 
 const trigger = event => stack.push(event)
 
 let prev
 const step = dt => {
+  // loop through events that happened since the last render
   while (stack.length) {
     let curr = stack.pop()
 
     if (prev) {
-      model.speed = distance(prev.x, prev.y, curr.x, curr.y)
+      let distanceInPct = distance(
+        prev.x / prev.sW, prev.y / prev.sH,
+        curr.x / curr.sW, curr.y / curr.sH
+      )
+      model.totalDistance += distanceInPct
     }
 
     prev = curr
   }
+
+  model.speed = model.totalDistance / model.totalTime || 0
+
+  model.totalTime += dt
 }
 
 module.exports = {
