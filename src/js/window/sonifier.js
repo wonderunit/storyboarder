6853e2f3b56c7fdeee68351dbbc8f0870f127198
Eric Skogen <audionerd@users.noreply.github.com>
2017-03-30 17:22:34 -0500

    eq and gain based on accel

--- src/js/window/sonifier.js
+++ src/js/window/sonifier.js
@@ -1,4 +1,5 @@
 const Tone = require("tone")
+const util = require('../utils/index')
 
 /*
     x
@@ -21,20 +22,29 @@ const loop = require('raf-loop')
 const instrument = (() => {
   const pathToSample = "./snd/drawing-loop.wav"
 
-  let env = new Tone.AmplitudeEnvelope({
-  	"attack": 0.05,
-  	"decay": 0.0,
-  	"sustain": 1.0,
-  	"release": 0.3
-  }).toMaster()
-
   let sampler = new Tone.Player(pathToSample)
     .set('loop', true)
     .set('retrigger', true)
     .set('volume', -20)
     .stop()
-    .connect(env)
-  
+
+  let eq = new Tone.EQ3()
+    .set({
+      low: -96,
+      lowFrequency: 2200
+    })
+
+  let env = new Tone.AmplitudeEnvelope({
+  	"attack": 0.025,
+  	"decay": 0.0,
+  	"sustain": 1.0,
+  	"release": 0.3
+  })
+
+  let gain = new Tone.Gain({ gain: 0 })
+
+  sampler.chain(eq, env, gain, Tone.Master)
+
   const start = () => {
     if (sampler.buffer.loaded) {
       const offset = Math.random() * sampler.buffer.duration
@@ -52,7 +62,10 @@ const instrument = (() => {
 
   return {
     start,
-    stop
+    stop,
+
+    hpFreq: eq.lowFrequency,
+    gain: gain.gain
   }
 })()
 
@@ -60,15 +73,17 @@ const distance = (x1, y1, x2, y2) =>
   Math.hypot(x2 - x1, y2 - y1)
 
 const createModel = () => ({
-  speed: 0,
   avgSpeed: 0,
 
   accel: 0,
 
   totalDistance: 0,
   totalTime: 0,
-  
-  damping: 0.9
+
+  damping: 0.2,
+
+  isMoving: false,
+  isAccel: false
 })
 
 let engine
@@ -77,52 +92,63 @@ let model
 
 const init = () => {
   engine = loop(step)
+
   stack = []
+  model = createModel()
+
+  engine.start()
 }
 
 const start = () => {
-  engine.start()
   stack = []
   model = createModel()
   instrument.start()
 }
 
 const stop = () => {
-  engine.stop()
   instrument.stop()
 }
 
-const trigger = event => stack.push(event)
+const trigger = event => curr = event
 
 let prev
+let curr
 const step = dt => {
   let frameSize = 1/60*1000 / dt
 
-  // loop through events that happened since the last render
-  while (stack.length) {
-    let curr = stack.pop()
+  const speed = prev ? distance(prev.x, prev.y, curr.x, curr.y) : 0
+  model.isMoving = speed > 0 ? true : false
 
-    if (prev) {
-      // NOTE unscaled distance in pixels
-      let d = distance(
-        prev.x, prev.y,
-        curr.x, curr.y
-      )
-      model.totalDistance += d
-      model.speed = d
-
-      model.accel += model.speed
-    }
-
-    prev = curr
-  }
+  model.accel += speed
+  model.accel *= frameSize * model.damping
+  if (model.accel < 0.0001) model.accel = 0
 
   model.avgSpeed = model.totalDistance / model.totalTime || 0
 
-  model.accel *= frameSize * model.damping
-  if (model.accel < 0.1) model.accel = 0
+  let wasAccel = model.isAccel
+  model.isAccel = (model.accel > 0.1) ? true : false
+  let changedAccel = model.isAccel != model.wasAccel
+
+  // TODO track if abs change of accel hits threshold, then ramp to new value
+  instrument.hpFreq.value = 200 + (model.accel * 40)
+  // instrument.hpFreq.cancelScheduledValues()
+  // instrument.hpFreq.rampTo(200 + (model.accel * 40), 0.01)
+
+  if (changedAccel) {
+    if (model.isAccel) {
+      const v = util.clamp(model.accel / 10, 0.25, 1)
+      instrument.gain.cancelScheduledValues()
+      instrument.gain.rampTo(
+        v,
+        0.01)
+    } else {
+      instrument.gain.cancelScheduledValues()
+      instrument.gain.rampTo(0, 0.05)
+    }
+  }
 
   model.totalTime += dt
+  prev = curr
 }
 
 module.exports = {
