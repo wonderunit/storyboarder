f132d2e914d5fc9c4e5657b1992eec7d09fd05ed
Eric Skogen <audionerd@users.noreply.github.com>
2017-03-31 13:49:29 -0500

    refactor prefs

--- src/js/prefs.js
+++ src/js/prefs.js
@@ -1,33 +1,38 @@
 const fs = require('fs')
 const path = require('path')
-const {app} = require('electron')
+const { app } = require('electron')
 
-let prefs
+module.exports = (() => {
+  let prefFile = path.join(app.getPath('userData'), 'pref.json')
 
-let prefModule = {
-  loadPrefs: function () {
-    console.log(app.getPath('userData'))
-    let prefFile = path.join(app.getPath('userData'), 'pref.json')
+  let prefs
+
+  const load = () => {
     try {
+      // load existing prefs
       prefs = JSON.parse(fs.readFileSync(prefFile))
     } catch (e) {
       //console.log(e)
       prefs = {}
 
       try {
-        fs.writeFileSync(prefFile, JSON.stringify(prefs))
+        // create new prefs
+        fs.writeFileSync(prefFile, JSON.stringify(prefs, null, 2))
       } catch (e) {
         //console.log(e)
       }
     }
-  },
-  savePrefs: function (prefs) {
-    let prefFile = path.join(app.getPath('userData'), '/pref.json')
-    fs.writeFileSync(prefFile, JSON.stringify(prefs))
-  },
-  getPrefs: function(){return prefs},
-}
+  }
 
-prefModule.loadPrefs()
+  const savePrefs = prefs =>
+    fs.writeFileSync(prefFile, JSON.stringify(prefs, null, 2))
 
-module.exports = prefModule
+  const getPrefs = () => prefs
+
+  load()
+
+  return {
+    savePrefs,
+    getPrefs
+  }
+})()
