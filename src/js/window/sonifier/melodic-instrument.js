5a096acb2fc7893ba304c9d5aeb894428d37cf17
Eric Skogen <audionerd@users.noreply.github.com>
2017-04-27 17:34:51 -0500

    split out arpeggiator logic

--- src/js/window/sonifier/melodic-instrument.js
+++ src/js/window/sonifier/melodic-instrument.js
@@ -4,8 +4,44 @@ const throttle = require('lodash.throttle')
 
 const util = require('../../utils')
 
+const chords = ["Amadd9", "GMadd9", "Bm7#5", "FMadd9", "Am7#5", "E7", "EMadd9", "G#m7#5", "EM", "Em#5"]
+
+let getNotes = () => tonal.chord(util.sample(chords))
+
+const Arpeggiator = onNext => {
+  let curr = 0
+  let list = []
+  const next = () => {
+    let index = ++curr % list.length
+    // if (index == 0) {
+    //   list = onNext()
+    // }
+    return list[index]
+  }
+  const reset = () => {
+    curr = 0
+  }
+  const setList = (value) => {
+    list = value
+  }
+  const getIndex = () => {
+    return curr
+  }
+
+  list = onNext()
+  return {
+    reset,
+    next,
+
+    setList,
+    getIndex
+  }
+}
+
 module.exports = () => {
-  const chords = ["Amadd9", "GMadd9", "Bm7#5", "FMadd9", "Am7#5", "E7", "EMadd9", "G#m7#5", "EM", "Em#5"]
+
+  let arp = Arpeggiator(getNotes)
+  arp.reset()
 
   let synth = new Tone.PolySynth(8, Tone.Synth)
     .set({
@@ -26,19 +62,19 @@ module.exports = () => {
         "type" : "sine"
       },
       "envelope" : {
-        "attack": 3,
-        "decay": 0.01,
+        "attack": 4,
+        "decay": 2,
         "sustain": 0.1,
-        "release": 7,
+        "release": 6,
       },
     })
 
   const synthFilter = new Tone.Filter(1250, "lowpass", -12)
-    .set('Q', 2)
+    .set('Q', 0)
 
   var synthVol = new Tone.Volume(-12)
 
-  var bassSynth2Vol = new Tone.Volume(-12)
+  var bassSynth2Vol = new Tone.Volume(-18)
 
   var verb = new Tone.Freeverb(0.96, 1000)
   verb.wet = 1
@@ -46,18 +82,15 @@ module.exports = () => {
   synth.chain(synthFilter, synthVol, verb, Tone.Master)
   bassSynth2.chain(bassSynth2Vol, verb, Tone.Master)
 
-  let currentNote
-  let currentChord
-
   let lastChangeAt = null
   let shouldTrigger = false
 
   const start = () => {
-    currentNote = 0
-    currentChord = util.sample(chords)
-    
     lastChangeAt = Date.now()
     shouldTrigger = true
+
+    arp.setList(getNotes())
+    arp.reset()
   }
 
   const stop = () => {
@@ -67,27 +100,26 @@ module.exports = () => {
   const trigger = (opt = { velocity: 1 }) => {
     const { velocity } = opt
 
-    // been at least 150 msecs since last change
+    // been at least n msecs since last change
     if (!shouldTrigger &&
         lastChangeAt && 
-        Date.now() - lastChangeAt > 1000) 
+        Date.now() - lastChangeAt > 1000)
     {
       lastChangeAt = Date.now()
       shouldTrigger = true
-      // reset
-      currentNote = 0
+      //
+      arp.reset()
     }
 
     if (!shouldTrigger) return
 
-    const chord = currentChord
-    const notes = tonal.chord(chord)
+    let startingIndex = arp.getIndex()
 
-    const note = util.sample(notes) + (Math.random() > 0.5 ? '3' : '4')
-    const onote = util.sample(notes) + (Math.random() > 0.5 ? '3' : '4')
-    const bnote = util.sample(notes) + (Math.random() > 0.5 ? '2' : '3')
+    const note  = arp.next() + (Math.random() > 0.5 ? '3' : '4')
+    const onote = arp.next() + (Math.random() > 0.5 ? '3' : '4')
+    const bnote = arp.next() + (Math.random() > 0.5 ? '2' : '3')
 
-    if (currentNote == 0) {
+    if (startingIndex == 0) {
       bassSynth2.triggerAttackRelease(Tone.Frequency(bnote).transpose(+12), "16n", undefined, 0.4)
     }
     if (velocity > 0.25) {
@@ -102,13 +134,15 @@ module.exports = () => {
       synth.triggerAttackRelease(Tone.Frequency(onote).transpose(+24), "16n", undefined, velocity * 0.5)
     }
 
-    currentNote++
     shouldTrigger = false
   }
 
   const triggerChange = () => {
     lastChangeAt = Date.now()
     shouldTrigger = true
+
+    // arp.setList(getNotes())
+    // arp.reset()
   }
 
   return {
