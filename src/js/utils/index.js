18f3a2c3971f2e80f4f8e8ae2fac6ae54a7fa62e
Eric Skogen <audionerd@users.noreply.github.com>
2017-06-28 14:25:05 -0500

    add zeroFill

--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -146,6 +146,20 @@ Object.equals = function( x, y ) {
   return true;
 }
 
+/**
+ * via https://github.com/feross/zero-fill
+ * Given a number, return a zero-filled string.
+ * From http://stackoverflow.com/questions/1267283/
+ * @param  {number} width
+ * @param  {number} number
+ * @return {string}
+ */
+const zeroFill = (width, number, pad = '0') => {
+  width -= number.toString().length
+  if (width > 0) return new Array(width + (/\./.test(number) ? 2 : 1)).join(pad) + number
+  return number + ''
+}
+
 module.exports = {
   msToTime,
   uidGen,
@@ -161,5 +175,6 @@ module.exports = {
   shuffle,
   compareNumbers,
   pluralize,
-  sample
+  sample,
+  zeroFill
 }
