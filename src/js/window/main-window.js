5c25dfee3c8efd83d37e2de9326a107952537115
Charles Forman <charles@setpixel.com>
2017-08-01 00:06:23 -0400

    flattened animated gif + captions + 888 width
    fixes #365

--- src/js/window/main-window.js
+++ src/js/window/main-window.js
@@ -3130,7 +3130,7 @@ let exportAnimatedGif = () => {
   notifications.notify({message: "Exporting " + boards.length + " boards. Please wait...", timing: 5})
   sfx.down()
   setTimeout(()=>{
-    exporter.exportAnimatedGif(boards, boardSize, 800, boardPath, true, boardData)
+    exporter.exportAnimatedGif(boards, boardSize, 888, boardPath, true, boardData)
   }, 1000)
 }
 
