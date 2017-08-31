70097443912ab3c6793509efd0693f9bfc69340d
Eric Skogen <audionerd@users.noreply.github.com>
2017-08-31 17:30:17 -0500

    thumbnails do not override board data opacity setting

--- src/js/window/main-window.js
+++ src/js/window/main-window.js
@@ -1490,8 +1490,6 @@ const renderThumbnailToNewCanvas = (index, options = { forceReadFromFiles: false
   if (!options.forceReadFromFiles && index == currentBoard) {
     // grab from memory
     canvasImageSources = storyboarderSketchPane.getCanvasImageSources()
-    // force reference opacity to default value
-    canvasImageSources[LAYER_INDEX_REFERENCE].opacity = exporterCommon.DEFAULT_REFERENCE_LAYER_OPACITY
     // render to context
     exporterCommon.flattenCanvasImageSourcesDataToContext(context, canvasImageSources, size)
     return Promise.resolve(canvas)
