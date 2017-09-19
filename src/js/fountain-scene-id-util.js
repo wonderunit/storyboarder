7d6dbb0b8b250b05f18179b64612c964bdb22906
Eric Skogen <audionerd@users.noreply.github.com>
2017-09-19 14:54:41 -0500

    cleanup

--- src/js/fountain-scene-id-util.js
+++ src/js/fountain-scene-id-util.js
@@ -3,7 +3,7 @@ const util = require('./utils/index')
 var regex = {
   scene_heading: /^((?:\*{0,3}_?)?(?:(?:int|ext|est|i\/e)[. ]).+)|^(?:\.(?!\.+))(.+)/i,
   scene_number: /( *#(.+)# *)/,
-};
+}
 
 // trim all newlines
 let trim = string => string.replace(/\r?\n|\r/, '')
@@ -17,34 +17,34 @@ let eol = string => {
 var insertSceneIds = function (script) {
   var src    = script.split(/\n/g)
     , i      = src.length, line, match, parts, text, meta, x, xlen, dual
-    , tokens = [];
+    , tokens = []
 
   let sceneCount = 0
 
-  let addedIds = false;
+  let addedIds = false
 
   for (var i = 0; i < src.length; i++) {
-    line = src[i];
+    line = src[i]
     
     // scene headings
     if (match = line.match(regex.scene_heading)) {
       sceneCount++
-      text = match[1] || match[2];
+      text = match[1] || match[2]
 
       if (text.indexOf('  ') !== text.length - 2) {
         if (meta = text.match(regex.scene_number)) {
-          meta = meta[2];
-          text = text.replace(regex.scene_number, '');
+          meta = meta[2]
+          text = text.replace(regex.scene_number, '')
         } else {
           src[i] = trim(src[i]) + ' #' + sceneCount + '-' + util.uidGen(5) + '#' + eol(src[i])
           addedIds = true
         }
       }
-      continue;
+      continue
     }
   }
-  return [src.join("\n"), addedIds];
-};
+  return [src.join("\n"), addedIds]
+}
 
 let fountainSceneIdUtil = {
   insertSceneIds: insertSceneIds,
