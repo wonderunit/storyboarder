330ffca8db07d80fb5d7cd7751cd86323004515d
Eric Skogen <audionerd@users.noreply.github.com>
2017-10-12 14:04:46 -0500

    sanity check for fountain scene id util

--- test/importers/fountain-scene-id-util.test.js
+++ test/importers/fountain-scene-id-util.test.js
@@ -25,5 +25,20 @@ describe('fountainSceneIdUtil', () => {
         done()
       })
     })
+    it('can handle an octothorpe', () => {
+      let rawText = 'EXT. A PLACE - DAY #1'
+
+      let [ scriptDataA, hasAddedA ] = fountainSceneIdUtil.insertSceneIds(rawText)
+      assert(hasAddedA)
+
+      let [ scriptDataB, hasAddedB ] = fountainSceneIdUtil.insertSceneIds(scriptDataA)
+
+      // doesn't need to change after first time through
+      assert(!hasAddedB)
+      assert.equal(scriptDataA, scriptDataB)
+
+      // adds the scene id after the octothorpe
+      assert(scriptDataA.includes('EXT. A PLACE - DAY #1 #1'))
+    })
   })
 })
