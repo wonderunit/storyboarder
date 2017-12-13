314dfaf9c10b8b7eefb6c8b8d11ea787c5746ef3
Eric Skogen <audionerd@users.noreply.github.com>
2017-12-13 12:27:22 -0600

    don't show filename on sidebar meta audio button

--- src/js/window/main-window.js
+++ src/js/window/main-window.js
@@ -2358,7 +2358,14 @@ let renderMetaData = () => {
   if (boardAudio) {
     // on
     audiofileInputEl.value = boardAudio.filename
-    audiofileTextEl.innerHTML = util.truncateMiddle(boardAudio.filename)
+
+    // audiofileTextEl.innerHTML = util.truncateMiddle(boardAudio.filename)
+    audiofileTextEl.innerHTML = '<span>' +
+                                  // '<span class="paren">(</span>' + 
+                                  'Audio' + // : 3s 44.1khz 16bit
+                                  // '<span class="paren">)</span>' +
+                                '</span>'
+
     audiofileSvgUseEl.setAttribute('xlink:href',
       audiofileSvgUseEl.getAttribute('xlink:href')
         .split('#')[0] + '#icon-speaker-on')
@@ -2366,9 +2373,9 @@ let renderMetaData = () => {
     // mute
     audiofileInputEl.value = ''
     audiofileTextEl.innerHTML = '<span class="muted">' +
-                                  '<span class="paren">(</span>' + 
-                                  '<span>no audio</span>' +
-                                  '<span class="paren">)</span>' +
+                                  // '<span class="paren">(</span>' + 
+                                  'Select Audio File' +
+                                  // '<span class="paren">)</span>' +
                                 '</span>'
     audiofileSvgUseEl.setAttribute('xlink:href',
       audiofileSvgUseEl.getAttribute('xlink:href')
