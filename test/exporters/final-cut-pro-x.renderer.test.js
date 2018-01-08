3e12ef3a1af3f52014903c5d7257784a1317d392
Eric Skogen <audionerd@users.noreply.github.com>
2018-01-08 14:22:53 -0600

    async tests

--- test/exporters/final-cut-pro-x.renderer.test.js
+++ test/exporters/final-cut-pro-x.renderer.test.js
@@ -58,29 +58,30 @@ let boardFileData = {
 }
 
 describe('exporters/final-cut-pro-x', () => {
-  const getXml = boardFileData => {
+  const getXml = async boardFileData => {
     let projectFileAbsolutePath = '/Users/me/projects/storyboarder/example\ storyboard/example\ storyboard.storyboarder'
     let outputPath = '/Users/me/projects/storyboarder/example\ storyboard/example\ storyboard.storyboarder/exports/output'
-    return exporterFcpX.generateFinalCutProXXml(exporterFcpX.generateFinalCutProXData(boardFileData, { projectFileAbsolutePath, outputPath }))
+    let data = await exporterFcpX.generateFinalCutProXData(boardFileData, { projectFileAbsolutePath, outputPath })
+    return exporterFcpX.generateFinalCutProXXml(data)
   }
-  it('can generate final cut pro x xml', () => {
+  it('can generate final cut pro x xml', async () => {
     let xml
 
-    assert.doesNotThrow(() => xml = getXml(boardFileData))
+    xml = await getXml(boardFileData)
 
     // check dash in filename
     assert(xml.includes('src="./example-storyboard-board-00001.png'))
 
     assert(xml.length > 32)
   })
-  it('can generate at 23.976 fps', () => {
+  it('can generate at 23.976 fps', async () => {
     boardFileData.fps = 23.976 // AKA 23.97602397 AKA 24000/1001
     boardFileData.boards[0].time      = 0
     boardFileData.boards[0].duration  = 29 / boardFileData.fps * 1000
     boardFileData.boards[1].time      = boardFileData.boards[0].duration
     boardFileData.boards[1].duration  = 31 / boardFileData.fps * 1000
 
-    xml = getXml(boardFileData)
+    xml = await getXml(boardFileData)
 
     // check fps calculations
     // <format id="r1" frameDuration="1001/24000s" width="900" height="900"/>
@@ -90,14 +91,14 @@ describe('exporters/final-cut-pro-x', () => {
     assert(xml.includes('<video name="1A" offset="0s" ref="r4" duration="29029/24000s" start="0s">'))
     assert(xml.includes('<video name="2A" offset="29029/24000s" ref="r5" duration="31031/24000s" start="0s">'))
   })
-  it('can generate at 24 fps', () => {
+  it('can generate at 24 fps', async () => {
     boardFileData.fps = 24
     boardFileData.boards[0].time      = 0
     boardFileData.boards[0].duration  = 29 / boardFileData.fps * 1000
     boardFileData.boards[1].time      = boardFileData.boards[0].duration
     boardFileData.boards[1].duration  = 31 / boardFileData.fps * 1000
 
-    xml = getXml(boardFileData)
+    xml = await getXml(boardFileData)
 
     // check fps calculations
     // <format id="r1" frameDuration="100/2400s" width="900" height="900"/>
@@ -107,14 +108,14 @@ describe('exporters/final-cut-pro-x', () => {
     assert(xml.includes('<video name="1A" offset="0s" ref="r4" duration="2900/2400s" start="0s">'))
     assert(xml.includes('<video name="2A" offset="2900/2400s" ref="r5" duration="3100/2400s" start="0s">'))
   })
-  it('can generate at 29.97 fps', () => {
+  it('can generate at 29.97 fps', async () => {
     boardFileData.fps = 29.97
     boardFileData.boards[0].time      = 0
     boardFileData.boards[0].duration  = 29 / boardFileData.fps * 1000
     boardFileData.boards[1].time      = boardFileData.boards[0].duration
     boardFileData.boards[1].duration  = 31 / boardFileData.fps * 1000
 
-    xml = getXml(boardFileData)
+    xml = await getXml(boardFileData)
 
     // check fps calculations
     // <format id="r1" frameDuration="100/2997s" width="900" height="900"/>
@@ -124,14 +125,14 @@ describe('exporters/final-cut-pro-x', () => {
     assert(xml.includes('<video name="1A" offset="0s" ref="r4" duration="2900/2997s" start="0s">'))
     assert(xml.includes('<video name="2A" offset="2900/2997s" ref="r5" duration="3100/2997s" start="0s">'))
   })
-  it('can generate at 59.94 fps', () => {
+  it('can generate at 59.94 fps', async () => {
     boardFileData.fps = 59.94
     boardFileData.boards[0].time      = 0
     boardFileData.boards[0].duration  = 29 / boardFileData.fps * 1000
     boardFileData.boards[1].time      = boardFileData.boards[0].duration
     boardFileData.boards[1].duration  = 31 / boardFileData.fps * 1000
 
-    xml = getXml(boardFileData)
+    xml = await getXml(boardFileData)
 
     // check fps calculations
     // <format id="r1" frameDuration="100/2997s" width="900" height="900"/>
