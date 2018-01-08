5291adf82700133d598b85589eb10932c6d39831
Eric Skogen <audionerd@users.noreply.github.com>
2018-01-08 14:40:13 -0600

    remove disclaimer that audio export was not supported

--- src/js/window/main-window.js
+++ src/js/window/main-window.js
@@ -4241,7 +4241,7 @@ const exportFcp = () => {
   sfx.down()
   setTimeout(()=>{
     exporter.exportFcp(boardData, boardFilename).then(outputPath => {
-      notifications.notify({message: "Your scene has been exported for Final Cut Pro X and Premiere. \n(Audio export is not supported in FCPX yet)", timing: 20})
+      notifications.notify({message: "Your scene has been exported for Final Cut Pro X and Premiere.", timing: 20})
       sfx.positive()
       shell.showItemInFolder(outputPath)
     }).catch(err => {
