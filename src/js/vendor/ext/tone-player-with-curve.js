ef60e405a3c5a3797c3c15aed482f7f6dc48a2e7
Eric Skogen <audionerd@users.noreply.github.com>
2018-01-19 13:45:50 -0600

    override correct version of Tone.Player._start
    fixes #978

--- src/js/vendor/ext/tone-player-with-curve.js
+++ src/js/vendor/ext/tone-player-with-curve.js
@@ -8,50 +8,50 @@ const Tone = require('tone')
 Tone.Player = class extends Tone.Player {
   constructor (url, onload) {
     super(url, onload)
-    this.curve = 'linear'
+    this.curve = 'linear' // ADDED for Storyboarder
   }
+
   _start (startTime, offset, duration) {
-    // if it's a loop the default offset is the loopstart point
-    if (this._loop) {
-      offset = Tone.defaultArg(offset, this._loopStart)
-    } else {
-      // otherwise the default offset is 0
-      offset = Tone.defaultArg(offset, 0)
-    }
+  	//if it's a loop the default offset is the loopstart point
+  	if (this._loop){
+  		offset = Tone.defaultArg(offset, this._loopStart);
+  	} else {
+  		//otherwise the default offset is 0
+  		offset = Tone.defaultArg(offset, 0);
+  	}
+  
+  	//compute the values in seconds
+  	offset = this.toSeconds(offset);
+  	duration = Tone.defaultArg(duration, Math.max(this._buffer.duration - offset, 0));
+  	duration = this.toSeconds(duration);
+  	startTime = this.toSeconds(startTime);
+  
+  	// //make the source
+  	this._source = new Tone.BufferSource({
+  		"buffer" : this._buffer,
+  		"loop" : this._loop,
+  		"loopStart" : this._loopStart,
+  		"loopEnd" : this._loopEnd,
+  		"playbackRate" : this._playbackRate,
+  		"fadeIn" : this.fadeIn,
+  		"fadeOut" : this.fadeOut,
 
-    // compute the values in seconds
-    offset = this.toSeconds(offset)
-    duration = Tone.defaultArg(duration, Math.max(this._buffer.duration - offset, 0))
-    duration = this.toSeconds(duration)
-    startTime = this.toSeconds(startTime)
+      "curve": this.curve // ADDED for Storyboarder
 
-    // make the source
-    var source = new Tone.BufferSource({
-      'buffer': this._buffer,
-      'loop': this._loop,
-      'loopStart': this._loopStart,
-      'loopEnd': this._loopEnd,
-      'playbackRate': this._playbackRate,
-      'fadeIn': this.fadeIn,
-      'fadeOut': this.fadeOut,
-      'curve': this.curve
-    }).connect(this.output)
-
-    // set the looping properties
-    if (!this._loop && !this._synced) {
-      // if it's not looping, set the state change at the end of the sample
-      this._state.setStateAtTime(Tone.State.Stopped, startTime + duration)
-    }
-
-    var event = this._state.get(startTime)
-    event.source = source
-
-    // start it
-    if (this._loop) {
-      source.start(startTime, offset)
-    } else {
-      source.start(startTime, offset, duration)
-    }
-    return this
-  }
+  	}).connect(this.output);
+  
+  	//set the looping properties
+  	if (!this._loop && !this._synced){
+  		//if it's not looping, set the state change at the end of the sample
+  		this._state.setStateAtTime(Tone.State.Stopped, startTime + duration);
+  	}
+  
+  	//start it
+  	if (this._loop){
+  		this._source.start(startTime, offset);
+  	} else {
+  		this._source.start(startTime, offset, duration);
+  	}
+  	return this;
+  };
 }
