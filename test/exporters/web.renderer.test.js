2eae6298283fb1a7479cb2b4e9cabaf16976198b
Eric Skogen <audionerd@users.noreply.github.com>
2018-02-01 17:35:19 -0600

    leave files in tmp after testing (for easier testing rn)

--- test/exporters/web.renderer.test.js
+++ test/exporters/web.renderer.test.js
@@ -1,4 +1,4 @@
-/* global describe before after it  */
+/* global describe it  */
 // electron-mocha --renderer test/exporters/web.renderer.test.js
 // find src/js/exporters/web.js test/exporters/web.renderer.test.js | entr -c electron-mocha --renderer test/exporters/web.renderer.test.js
 
@@ -10,23 +10,14 @@ const tmp = require('tmp')
 window.TONE_SILENCE_VERSION_LOGGING = true
 const exporterWeb = require('../../src/js/exporters/web')
 
-let tmpFolder
-describe('exporters/ffmpeg', () => {
-  before(function () {
-    tmpFolder = tmp.dirSync({ unsafeCleanup: true })
-  })
-
-  after(function () {
-    tmpFolder.removeCallback()
-  })
-
+describe('exporters/web', () => {
   it('can export web-friendly files for a scene', async () => {
     let sceneFilePath = path.join(__dirname, '..', 'fixtures', 'audio', 'audio.storyboarder')
 
     // load scene data
     // let scene = JSON.parse(fs.readFileSync(sceneFilePath))
 
-    let tmpFolder = tmp.dirSync({ unsafeCleanup: true })
+    let tmpFolder = tmp.dirSync({ unsafeCleanup: false })
     let outputPath = path.join(tmpFolder.name, 'output')
     fs.mkdirSync(outputPath)
 
@@ -34,5 +25,10 @@ describe('exporters/ffmpeg', () => {
       sceneFilePath,
       outputPath
     )
+
+    console.log('\n\n\n')
+    console.log('Test files written to:')
+    console.log(outputPath)
+    console.log('\n\n\n')
   })
 })
