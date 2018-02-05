33dbb711fdd9c17207a2517ff79c43820bfc1c6d
Eric Skogen <audionerd@users.noreply.github.com>
2018-02-05 14:20:06 -0600

    bg color for audio beyond board duration

--- src/js/window/scene-timeline-view.js
+++ src/js/window/scene-timeline-view.js
@@ -795,7 +795,7 @@ class TimelineView {
           {
             // lanes container
             style: `position: relative;
-                    background-color: #333;`
+                    background-color: #444;`
           },
           [
             boardLane,
