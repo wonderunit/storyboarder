0649d8f1fd67d34c2cf817a04230e0e5df9c3363
Eric Skogen <audionerd@users.noreply.github.com>
2018-05-22 11:48:50 -0500

    comment re: mtime

--- src/js/window/main-window.js
+++ src/js/window/main-window.js
@@ -179,6 +179,7 @@ const framesToMsecs = value => Math.round(value / boardData.fps * 1000)
 
 // TODO better name than etags?
 // TODO store in boardData instead, but exclude from JSON?
+// TODO use mtime trick like we do for layers and posterframes?
 // cache buster for thumbnails
 let etags = {}
 const setEtag = absoluteFilePath => { etags[absoluteFilePath] = Date.now() }
