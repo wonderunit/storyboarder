631d60b82e8ecb943ba5d75325c603841cee9f4b
Eric Skogen <audionerd@users.noreply.github.com>
2018-07-17 14:31:41 -0500

    extract fetchWithTimeout to utils

---
+++ src/js/utils/fetchWithTimeout.js
@@ -0,0 +1,34 @@
+// via https://davidwalsh.name/fetch-timeout
+function fetchWithTimeout (input, init = {}, timeoutInMsecs = 10000, fetcher) {
+  return new Promise(function (resolve, reject) {
+    let elapsed = false
+
+    const timeout = setTimeout(
+      function () {
+        elapsed = true
+        reject(new Error('Request timed out'))
+      },
+      timeoutInMsecs
+    )
+
+    fetcher(input, init)
+      .then(function (response) {
+        // cleanup the timeout
+        clearTimeout(timeout)
+
+        // if setTimeout hasn't rejected yet
+        if ( !elapsed ) {
+          resolve(response)
+        }
+      })
+      .catch(function (err) {
+        // if setTimeout already rejected
+        if (elapsed) return
+
+        // otherwise, reject with error
+        reject(err)
+      })
+    })
+}
+
+module.exports = fetchWithTimeout
