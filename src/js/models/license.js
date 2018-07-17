70811a4d7e3af19912b17bb426d1203070133d15
Eric Skogen <audionerd@users.noreply.github.com>
2018-07-17 14:25:19 -0500

    basic license checking

---
+++ src/js/models/license.js
@@ -0,0 +1,158 @@
+const { machineIdSync } = require('node-machine-id')
+
+const pkg = require('../../../package.json')
+
+const VERIFICATION_URL = new URL('http://example.com/licenses/verify')
+// node doesn't have `URL`
+// so, to run in `main` instead of `renderer`:
+//
+// let VERIFICATION_URL = {
+//   origin: 'http://example.com',
+//   pathname: '/licenses/verify'
+// }
+// VERIFICATION_URL.toString = () => `${VERIFICATION_URL.origin}${VERIFICATION_URL.pathname}`
+
+// via https://davidwalsh.name/fetch-timeout
+function fetchWithTimeout(input, init = {}, timeoutInMsecs = 10000, fetcher) {
+  return new Promise(function (resolve, reject) {
+    let elapsed = false
+
+    const timeout = setTimeout(
+      function () {
+        elapsed = true
+        reject(new Error('Request timed out'))
+      },
+      timeoutInMsecs
+    )
+
+    fetcher(input, init)
+      .then(function (response) {
+        // cleanup the timeout
+        clearTimeout(timeout)
+
+        // if setTimeout hasn't rejected yet
+        if ( !elapsed ) {
+          resolve(response)
+        }
+      })
+      .catch(function (err) {
+        // if setTimeout already rejected
+        if (elapsed) return
+
+        // otherwise, reject with error
+        reject(err)
+      })
+    })
+}
+
+// checkLicense
+//
+// e.g.:
+//
+// if (checkLicense(license)) {
+//   // unlock features
+// } else {
+//   // delete token file (if exists)
+//   // notify user they are not licensed
+// }
+//
+async function checkLicense (
+  license,
+  options = {}
+) {
+  let defaultOptions = {
+    verificationUri: VERIFICATION_URL.toString(),
+    userAgent: `Storyboarder/${pkg.version}`,
+    timeoutInMsecs: 10000
+  }
+  options = { ...defaultOptions, ...options }
+  try {
+    // get expectedMachineId
+    let expectedMachineId = machineIdSync({
+      // compare original values (not SHA-256'd)
+      original: true
+    })
+
+    // is there a license object?
+    if (license) {
+      // does the license machineId match the expected machineId?
+      if (
+        // machineId is a string
+        typeof license.machineId === 'string' &&
+        // machineId is not empty
+        license.machineId.length > 4 &&
+        // they are an exact match
+        license.machineId === expectedMachineId
+      ) {
+        // is the token still fresh?
+        if (
+          license.licenseExpiration >= Date.now().valueOf() / 1000
+        ) {
+          // try checking the server
+          // if valid, continue
+          try {
+            let response = await fetchWithTimeout(
+              options.verificationUri,
+              {
+                method: 'POST',
+                cache: 'no-cache',
+                headers: {
+                  'User-Agent': options.userAgent,
+                  'Content-Type': 'application/json; charset=utf-8',
+                },
+                body: JSON.stringify(license)
+              },
+              options.timeoutInMsecs,
+              options.fetcher
+            )
+            if (response.status === 200) {
+              console.log('license check ok')
+              return true
+            } else {
+              console.log('license check failed: invalidated by server')
+              return false
+            }
+
+          } catch (err) {
+            console.log('license check: server error')
+            console.log(err.stack)
+            console.log('code:', err.code)
+            console.log('message', err.message)
+            // TODO why does electron-mocha not print an error string here?
+            // to see error: npx floss -d -p test/models/license.renderer.test.js
+            console.error(err)
+
+            if (err.code === 'ENETUNREACH') {
+              console.log('license check: server unreachable')
+              return true
+            } else if (err.message === 'Request timed out') {
+              console.log('license check: request timed out')
+              return true
+            } else {
+              return true
+            }
+          }
+        } else {
+          console.log('license check failed: expired')
+          return false
+        }
+      } else {
+        console.log('license check failed: machine id mis-match')
+        return false
+      }
+    } else {
+      console.log('license check failed: no license')
+      return false
+    }
+  } catch (err) {
+    console.log('license check failed: unexpected error')
+    console.log(err)
+    console.error(err)
+    return false
+  }
+}
+
+module.exports = {
+  VERIFICATION_URL,
+  checkLicense
+}
---
+++ test/models/license.renderer.test.js
@@ -0,0 +1,102 @@
+/* global describe it */
+
+// USAGE npx electron-mocha --renderer test/license.renderer.test.js
+
+const { machineIdSync } = require('node-machine-id')
+
+const assert = require('assert')
+const nock = require('nock')
+const nodeFetch = require('node-fetch') // because nock doesn’t work with fetch in electron-mocha
+const R = require('ramda')
+
+const { VERIFICATION_URL, checkLicense } = require('../../src/js/models/license')
+
+// use node-fetch as the fetcher in our tests so that we can use nock
+let modified = {
+  checkLicense: R.partialRight(checkLicense, [{ fetcher: nodeFetch }])
+}
+
+describe('license', () => {
+  let testMachineId = machineIdSync({ original: true })
+  let now = () => Date.now().valueOf() / 1000
+  let validLicense
+
+  beforeEach(() => {
+    nock.disableNetConnect()
+
+    validLicense = {
+      licenseExpiration: now() + 1000,
+      machineId: testMachineId
+    }
+  })
+
+  afterEach(() => {
+    nock.cleanAll()
+    nock.enableNetConnect()
+  })
+
+  describe('with basic server approval', () => {
+    let scope
+
+    beforeEach(() => {
+      scope = nock(VERIFICATION_URL.origin)
+        .post(VERIFICATION_URL.pathname)
+        .reply(200)
+    })
+
+    it('can be checked', async () => {
+      assert.equal(true, await modified.checkLicense(validLicense))
+    }).timeout(0)
+
+    it('requires a license object', async () => {
+      assert.equal(false, await modified.checkLicense({}))
+    })
+
+    it('requires a matching machine id', async () => {
+      assert.equal(false, await modified.checkLicense({
+        ...validLicense,
+        machineId: 'fake id',
+      }))
+    })
+
+    it('requires a fresh expiration date', async () => {
+      assert.equal(false, await modified.checkLicense({
+        ...validLicense,
+        licenseExpiration: now() - 10000,
+      }))
+    })
+  })
+
+  describe('server checks', () => {
+    let scope
+
+    it('if server can be reached, fails without server approval', async () => {
+      let scope = nock(VERIFICATION_URL.origin)
+        .post(VERIFICATION_URL.pathname)
+        .reply(402)
+      assert.equal(false, await modified.checkLicense(validLicense))
+      scope.done()
+    })
+
+    it('passes anyway if server times out', async () => {
+      let scope = nock(VERIFICATION_URL.origin)
+        .post(VERIFICATION_URL.pathname)
+        .delay(30000)
+        .reply(500)
+    
+      assert(await modified.checkLicense(validLicense, { timeoutInMsecs: 250 }))
+    }).timeout(0)
+
+    it('passes anyway if server is inaccessible', async () => {
+      // TODO replace with simpler assert.rejects (only available in node v10.0.0+)
+      try {
+        await nodeFetch(VERIFICATION_URL.toString())
+        assert.fail()
+      } catch (err) {
+        console.error(err)
+        assert(err.code === 'ENETUNREACH')
+      }
+      assert(await modified.checkLicense(validLicense))
+    }).timeout(0)
+  })
+})
