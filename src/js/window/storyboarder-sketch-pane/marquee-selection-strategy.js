dc3141e2c8cbfdad83e6267a889e57e34cd8209a
Eric Skogen <audionerd@users.noreply.github.com>
2018-09-14 16:35:40 -0500

    marquee: switch between freeform and line via hotkey while selecting
    extract MarqueeSelectionStrategy to its own file

---
+++ src/js/window/storyboarder-sketch-pane/marquee-selection-strategy.js
@@ -0,0 +1,218 @@
+const paper = require('paper')
+
+class MarqueeSelectionStrategy {
+  constructor (context) {
+    this.context = context
+    this.name = 'marqueeSelection'
+
+    this._onPointerDown = this._onPointerDown.bind(this)
+    this._onPointerMove = this._onPointerMove.bind(this)
+    this._onPointerUp = this._onPointerUp.bind(this)
+    this._onKeyDown = this._onKeyDown.bind(this)
+    this._onKeyUp = this._onKeyUp.bind(this)
+
+    this._onWindowBlur = this._onWindowBlur.bind(this)
+
+    this.offscreenCanvas = document.createElement('canvas')
+    this.offscreenContext = this.offscreenCanvas.getContext('2d')
+  }
+
+  startup () {
+    this.context.store.dispatch({ type: 'TOOLBAR_MODE_STATUS_SET', payload: 'busy', meta: { scope: 'local' } })
+
+    this.offscreenCanvas.width = this.context.sketchPane.width
+    this.offscreenCanvas.height = this.context.sketchPane.height
+    this.layer = this.context.sketchPane.layers.findByName('composite')
+
+    this.state = {
+      started: false,
+      complete: false,
+      selectionPath: new paper.Path(),
+      draftPoint: null,
+      straightLinePressed: false,
+      isPointerDown: false,
+
+      stateName: 'freeform' // freeform or line
+    }
+
+    this.context.sketchPaneDOMElement.addEventListener('pointerdown', this._onPointerDown)
+    document.addEventListener('pointermove', this._onPointerMove)
+    document.addEventListener('pointerup', this._onPointerUp)
+    window.addEventListener('keydown', this._onKeyDown)
+    window.addEventListener('keyup', this._onKeyUp)
+    window.addEventListener('blur', this._onWindowBlur)
+
+    this.context.sketchPane.cursor.setEnabled(false)
+    this.context.sketchPane.app.view.style.cursor = 'crosshair'
+  }
+
+  shutdown () {
+    this.context.sketchPaneDOMElement.removeEventListener('pointerdown', this._onPointerDown)
+    document.removeEventListener('pointermove', this._onPointerMove)
+    document.removeEventListener('pointerup', this._onPointerUp)
+    window.removeEventListener('keydown', this._onKeyDown)
+    window.removeEventListener('keyup', this._onKeyUp)
+    window.removeEventListener('blur', this._onWindowBlur)
+
+    this.context.sketchPane.app.view.style.cursor = 'auto'
+    this.context.sketchPane.cursor.setEnabled(true)
+  }
+
+  _isLineKeyPressed () {
+    return this.context.isCommandPressed('drawing:marquee:straight-line')
+  }
+
+  _onPointerDown (event) {
+    // if this is a new path
+    if (!this.state.started) {
+
+      // reset
+      this.state.selectionPath = new paper.Path()
+
+      // if the line key is pressed
+      if (this._isLineKeyPressed()) {
+        this.state.stateName = 'line'
+      } else {
+        this.state.stateName = 'freeform'
+      }
+    }
+
+    this.state = {
+      ...this.state,
+      started: true,
+      complete: false,
+      isPointerDown: true
+    }
+
+    this._addPointFromEvent(event)
+    this._draw()
+
+    if (this.state.stateName == 'line') {
+      this.state.draftPoint = this.context.sketchPane.localizePoint(event)
+    } else {
+      this.state.draftPoint = null
+    }
+  }
+
+  _onPointerMove (event) {
+    if (!this.state.started) return
+
+    if (!this._isLineKeyPressed() && this.state.isPointerDown) {
+      this.state.stateName = 'freeform'
+    }
+
+    if (this.state.stateName == 'line') {
+      this.state.draftPoint = this.context.sketchPane.localizePoint(event)
+    } else {
+      this.state.draftPoint = null
+      this._addPointFromEvent(event)
+    }
+
+    this._draw()
+  }
+
+  _onPointerUp (event) {
+    this.state.isPointerDown = false
+
+    if (!this.state.started) return
+
+    if (this._isLineKeyPressed()) {
+      this.state.stateName = 'line'
+    } else {
+      this.state.stateName = 'freeform'
+    }
+
+    if (this.state.stateName == 'line') {
+      this._addPointFromEvent(event)
+      this._draw()
+    } else {
+      this._addPointFromEvent(event)
+
+      this._complete()
+    }
+  }
+
+  _complete () {
+    this.state.started = false
+    this.state.complete = true
+
+    this.state.draftPoint = null
+
+    // close the path
+    this.state.selectionPath.add(this.state.selectionPath.segments[0].point.clone())
+    // avoid self-intersections
+    // this.state.selectionPath = this.state.selectionPath.unite(this.state.selectionPath)
+    // this.state.selectionPath.closePath()
+
+    this._draw()
+
+    this.context.marqueePath = this.state.selectionPath.clone()
+
+    // this.context.store.dispatch({
+    //   type: 'TOOLBAR_MODE_STATUS_SET', payload: 'idle', meta: { scope: 'local' }
+    // })
+    // this.context.store.dispatch({
+    //   type: 'TOOLBAR_MODE_SET', payload: 'marqueeOperation', meta: { scope: 'local' }
+    // })
+  }
+
+  _addPointFromEvent (event) {
+    let point = this.context.sketchPane.localizePoint(event)
+    this.state.selectionPath.add(new paper.Point(point.x, point.y))
+  }
+
+  _onWindowBlur () {
+    // attempt to gracefully transition back to drawing
+    this.context.store.dispatch({ type: 'TOOLBAR_MODE_SET', payload: 'drawing', meta: { scope: 'local' } })
+  }
+
+  _onKeyDown (event) {
+    //
+  }
+
+  _onKeyUp (event) {
+    if (!this._isLineKeyPressed()) {
+      if (this.state.started && this.state.stateName == 'line' && !this.state.isPointerDown) {
+        this._complete()
+      }
+    }
+  }
+
+  _draw () {
+    let ctx = this.offscreenContext
+
+    ctx.clearRect(0, 0, this.context.sketchPane.width, this.context.sketchPane.height)
+    ctx.globalAlpha = 1.0
+
+    let pointsToDraw = this.state.selectionPath.segments.map(segment => ({ x: segment.point.x, y: segment.point.y }))
+    if (this.state.draftPoint != null) {
+      pointsToDraw.push(this.state.draftPoint)
+    }
+
+    ctx.save()
+    ctx.strokeStyle = '#f00'
+    ctx.beginPath()
+    ctx.moveTo(pointsToDraw[0].x, pointsToDraw[0].y)
+    for (let i = 1; i < pointsToDraw.length; i++) {
+      let point = pointsToDraw[i]
+      ctx.lineTo(point.x, point.y)
+    }
+    ctx.closePath()
+    ctx.stroke()
+    ctx.restore()
+
+    // diagnostic circles:
+    //
+    // for (let j = 0; j < pointsToDraw.length; j++) {
+    //   let point = pointsToDraw[j]
+    //   ctx.beginPath()
+    //   ctx.arc(point.x, point.y, 10, 0, Math.PI * 2)
+    //   ctx.fillStyle = '#f00'
+    //   ctx.fill()
+    // }
+
+    this.layer.replaceTextureFromCanvas(this.offscreenCanvas)
+  }
+}
+
+module.exports = MarqueeSelectionStrategy
