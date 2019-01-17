ca81569b45a335d2560f4464c8db9cd05cb45b52
Eric Skogen <audionerd@users.noreply.github.com>
2019-01-17 15:31:14 -0600

    cleanup

--- src/js/shot-generator/CameraControls.js
+++ src/js/shot-generator/CameraControls.js
@@ -12,15 +12,6 @@ class CameraControls {
     this.movementSpeed = .001
     this.maxSpeed = 0.05
 
-    this.mouse = {
-      moveForward : false,
-      moveLeft : false,
-      moveBackward : false,
-      moveRight : false,
-      moveUp : false,
-      moveDown : false
-    }
-
     this.onPointerMove = this.onPointerMove.bind(this)
     this.onPointerDown = this.onPointerDown.bind(this)
     this.onPointerUp = this.onPointerUp.bind(this)
@@ -81,7 +72,7 @@ class CameraControls {
       case 68: /*D*/
       case 82: /*R*/
       case 70: /*F*/
-        if ( this.mouse.moveForward || this.mouse.moveBackward || this.mouse.moveLeft || this.mouse.moveRight || this.mouse.moveUp || this.mouse.moveDown) {
+        if ( this.moveForward || this.moveBackward || this.moveLeft || this.moveRight || this.moveUp || this.moveDown) {
         } else {
           this.movementSpeed = .0001
         }
@@ -90,30 +81,30 @@ class CameraControls {
 
     switch ( event.keyCode ) {
       case 38: /*up*/
-      case 87: /*W*/ this.mouse.moveForward = true; break
+      case 87: /*W*/ this.moveForward = true; break
       case 37: /*left*/
-      case 65: /*A*/ this.mouse.moveLeft = true; break
+      case 65: /*A*/ this.moveLeft = true; break
       case 40: /*down*/
-      case 83: /*S*/ this.mouse.moveBackward = true; break
+      case 83: /*S*/ this.moveBackward = true; break
       case 39: /*right*/
-      case 68: /*D*/ this.mouse.moveRight = true; break
-      case 82: /*R*/ this.mouse.moveUp = true; break
-      case 70: /*F*/ this.mouse.moveDown = true; break
+      case 68: /*D*/ this.moveRight = true; break
+      case 82: /*R*/ this.moveUp = true; break
+      case 70: /*F*/ this.moveDown = true; break
     }
   }
 
   onKeyUp ( event ) {
     switch ( event.keyCode ) {
       case 38: /*up*/
-      case 87: /*W*/ this.mouse.moveForward = false; break;
+      case 87: /*W*/ this.moveForward = false; break;
       case 37: /*left*/
-      case 65: /*A*/ this.mouse.moveLeft = false; break;
+      case 65: /*A*/ this.moveLeft = false; break;
       case 40: /*down*/
-      case 83: /*S*/ this.mouse.moveBackward = false; break;
+      case 83: /*S*/ this.moveBackward = false; break;
       case 39: /*right*/
-      case 68: /*D*/ this.mouse.moveRight = false; break;
-      case 82: /*R*/ this.mouse.moveUp = false; break;
-      case 70: /*F*/ this.mouse.moveDown = false; break;
+      case 68: /*D*/ this.moveRight = false; break;
+      case 82: /*R*/ this.moveUp = false; break;
+      case 70: /*F*/ this.moveDown = false; break;
     }
   }
 
