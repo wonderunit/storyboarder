d65c94e13d324b87e3b979f543792d421df06672
Eric Skogen <audionerd@users.noreply.github.com>
2019-01-17 15:20:53 -0600

    add dualshock controller test

---
+++ test/shot-generator/controller/index.js
@@ -0,0 +1,59 @@
+const electron = require('electron')
+const { ipcMain } = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+const isDev = require('electron-is-dev')
+
+const path = require('path')
+const url = require('url')
+const os = require('os')
+
+let mainWindow
+
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = true
+
+function createWindow () {
+  console.log('new window', __dirname)
+  mainWindow = new BrowserWindow({
+    width: 500,
+    height: 560,
+    // x: 0,
+    // y: 0,
+    show: false,
+    center: true,
+    frame: true,
+    titleBarStyle: 'default', //'hiddenInset',
+    acceptFirstMouse: true,
+    simpleFullscreen: true,
+    webPreferences: {
+      plugins: true,
+      webSecurity: false,
+      allowRunningInsecureContent: true,
+      experimentalFeatures: true,
+      backgroundThrottling: true,
+    }
+  })
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+
+  mainWindow.once('ready-to-show', () => {
+    if (!isDev) autoUpdater.init()
+    if (isDev) mainWindow.webContents.openDevTools()
+    mainWindow.webContents.send('ready')
+    mainWindow.show()
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  app.quit()
+})
