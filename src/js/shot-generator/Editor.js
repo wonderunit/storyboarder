29b3c9f14e417864512531ae074bcb560a2e6f6d
Eric Skogen <audionerd@users.noreply.github.com>
2019-01-24 14:32:09 -0600

    extract NumberSlider to file

--- src/js/shot-generator/Editor.js
+++ src/js/shot-generator/Editor.js
@@ -71,6 +71,7 @@ const presetsStorage = require('../shared/store/presetsStorage')
 
 const WorldObject = require('./World')
 
+const NumberSlider = require('./NumberSlider')
 
 require('../vendor/OutlineEffect.js')
 const RoundedBoxGeometry = require('three-rounded-box')(THREE)
@@ -1144,51 +1145,6 @@ const ElementsPanel = connect(
       )
   }
 ))
-
-const NumberSlider = React.memo(({ label, value, onSetValue, min, max, step, formatter }) => {
-  const [fine, setFine] = useState(false)
-
-  min = min == null ? -10 : min
-  max = max == null ? 10 : max
-  step = step == null ? 0.01 : step
-
-  const onChange = event => {
-    event.preventDefault()
-    if (fine) {
-      let change = parseFloat(event.target.value) - value
-      onSetValue(value + (change / 1000))
-    } else {
-      onSetValue(parseFloat(event.target.value))
-    }
-  }
-
-  formatter = formatter != null
-    ? formatter
-    : value => value.toFixed(2)
-
-  useEffect(() => {
-    const onKeyDown = event => {
-      setFine(event.altKey)
-      if (event.key === 'Escape') {
-        document.activeElement.blur()
-      }
-    }
-    window.addEventListener('keydown', onKeyDown)
-    window.addEventListener('keyup', onKeyDown)
-    return function cleanup () {
-      window.removeEventListener('keydown', onKeyDown)
-      window.removeEventListener('keyup', onKeyDown)
-    }
-  }, [])
-
-  return h([
-    'div.number-slider', { style: { display: 'flex', flexDirection: 'row' } }, [
-      ['div', { style: { width: 50 } }, label],
-      ['input', { style: { flex: 1 }, type: 'range', onChange, min, max, step, value }],
-      ['div', { style: { width: 40 } }, formatter(value)]
-    ]
-  ])
-})
 
 const LabelInput = ({ label, setLabel, onFocus, onBlur }) => {
   const [editing, setEditing] = useState(false)
