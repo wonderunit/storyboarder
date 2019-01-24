bf72be0fab0d02820bf94667d870c72e9707a95c
Eric Skogen <audionerd@users.noreply.github.com>
2019-01-24 11:56:01 -0600

    add test cases

--- test/components/number-slider/index.js
+++ test/components/number-slider/index.js
@@ -12,26 +12,84 @@ document.body.appendChild(container)
 let style = document.createElement('style')
 style.type = 'text/css'
 style.appendChild(document.createTextNode(`
+  body {
+    font-size: 12px;
+    font-family: "Helvetica Neue";
+  }
+  body > div {
+    width: 300px;
+  }
   .number-slider {
     display: flex;
     flex-direction: row;
+    margin-bottom: 0.5rem;
   }
   .number-slider__label {
-    width: 50px;
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    cursor: default;
+    padding: 0 0.5rem 0 0;
+    margin-top: -1px; /* optical */
   }
   .number-slider__input {
     flex: 1;
+    cursor: col-resize;
+    text-align: center;
+    border: 0;
+    outline: 0;
+    padding: 6px;
   }
-  .number-slider__value {
-    width: 40px;
+  .number-slider__input--text {
+    background-color: yellow;
+  }
+  .number-slider__input--move {
+    background-color: #eee;
   }
 `))
 document.head.appendChild(style)
 
-const NumberSliderTest = () => {
-  const [value, setValue] = useState(0)
+const formatters = {
+  degrees: value => value.toFixed(0) + '°',
+  // formatter: value => feetAndInchesAsString(...metersAsFeetAndInches(sceneObject.height))
+  percent: value => Math.round(value * 100).toString() + '%'
+  // radToDeg: value => Math.round(value * THREE.Math.RAD2DEG).toString() + '°'
+}
 
-  return React.createElement(NumberSlider, { label: 'Number', value, onSetValue: setValue })
+const NumberSliderTest = () => {
+  const [number, setNumber] = useState(0)
+  const [degree, setDegree] = useState(0)
+  const [percent, setPercent] = useState(0)
+
+  return React.createElement('div', null, [
+    React.createElement(NumberSlider, {
+      key: 0,
+      label: 'Number',
+      value: number,
+      onSetValue: setNumber
+    }),
+    React.createElement(NumberSlider, {
+      key: 1,
+      label: 'Degrees',
+      value: degree,
+      min: -360,
+      max: 360,
+      step: 1,
+      onSetValue: setDegree,
+      formatter: formatters.degrees
+    }),
+    React.createElement(NumberSlider, {
+      key: 2,
+      label: 'Percent',
+      value: percent,
+      min: 0,
+      max: 1,
+      step: 1/100,
+      onSetValue: setPercent,
+      formatter: formatters.percent
+    })
+  ])
 }
 
 ReactDOM.render(
