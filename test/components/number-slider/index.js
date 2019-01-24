77393f0e23f218d6f4415ec4f5f72aedf9176a2d
Eric Skogen <audionerd@users.noreply.github.com>
2019-01-24 15:09:27 -0600

    update ui test

--- test/components/number-slider/index.js
+++ test/components/number-slider/index.js
@@ -4,91 +4,26 @@ const { useState } = React = require('react')
 const ReactDOM = require('react-dom')
 console.clear()
 
-const NumberSlider = require('./NumberSlider')
+const NumberSlider = require('../../../src/js/shot-generator/NumberSlider')
 
 const container = document.createElement('div')
 document.body.appendChild(container)
 
+const addLink = filepath => {
+  var styles = document.createElement('link')
+  styles.rel = 'stylesheet'
+  styles.type = 'text/css'
+  styles.href = filepath
+  document.head.appendChild(styles)
+}
+addLink('../../../src/css/fonts.css')
+addLink('../../../src/css/shot-generator.css')
+
 let style = document.createElement('style')
 style.type = 'text/css'
 style.appendChild(document.createTextNode(`
-  body {
-    font-size: 12px;
-    font-family: "Helvetica Neue";
-  }
   body > div {
     width: 300px;
-  }
-  .number-slider {
-    position: relative;
-    margin-bottom: 0.5rem;
-    display: flex;
-    flex-direction: row;
-  }
-  .number-slider__label {
-    flex: 1;
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    cursor: default;
-    padding: 0 0.5rem 0 0;
-    margin-top: -1px; /* optical */
-  }
-  .number-slider__control {
-    display: flex;
-    flex-direction: row;
-    background-color: #eee;
-  }
-  .number-slider__input {
-    flex: 1;
-    cursor: col-resize;
-    text-align: center;
-    border: 0;
-    outline: 0;
-    padding: 6px;
-    background-color: #eee;
-  }
-  .number-slider__input--text {
-    background-color: yellow;
-  }
-  .number-slider__input--move {
-  }
-  .number-slider__nudge {
-    position: absolute;
-    display: block;
-    width: 1.5rem;
-    height: 100%;
-    background-color: #ddd;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  .number-slider__nudge--right {
-    right: 0;
-  }
-  .number-slider__nudge {
-    opacity: 0;
-    transition: opacity 0.125s ease-out;
-  }
-  .number-slider:hover .number-slider__nudge {
-    opacity: 1;
-  }
-  .number-slider__arrow {
-    width: 0;
-    height: 0;
-    position: relative;
-  }
-  .number-slider__arrow--left {
-    border-top: 0.31rem solid transparent;
-    border-bottom: 0.31rem solid transparent;
-    border-right: 0.31rem solid black;
-    left: -1px; /* optical */
-  }
-  .number-slider__arrow--right {
-    border-top: 0.31rem solid transparent;
-    border-bottom: 0.31rem solid transparent;
-    border-left: 0.31rem solid black;
-    left: 1px; /* optical */
   }
 `))
 document.head.appendChild(style)
@@ -123,9 +58,9 @@ const NumberSliderTest = () => {
       step: 1,
       onSetValue: setDegree,
       formatter: formatters.degrees,
-      transform: (prev, delta, { min, max, step }) => {
+      transform: (prev, delta, { min, max, step, fine }) => {
         // inc/dec
-        let value = prev + delta * step
+        let val = prev + delta * (step * (fine ? 0.01 : 1))
         // mod
         if (value > 180) { return value - 360 }
         if (value < -180) { return value + 360 }
