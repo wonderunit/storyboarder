314ab53588140964df5f22a5ae6b2f8ecbde8d1f
Eric Skogen <audionerd@users.noreply.github.com>
2019-01-30 13:54:37 -0600

    fade in

--- src/js/shot-generator/ServerInspector.js
+++ src/js/shot-generator/ServerInspector.js
@@ -11,14 +11,14 @@ const ServerInspector = connect(
 )(({ server }) => {
   const buffer = useMemo(() =>
     server.uri
-      ? qr.imageSync(server.uri, { ec_level: 'H', type: 'png', size: 6, margin: 0 })
+      ? qr.imageSync(server.uri, { ec_level: 'H', type: 'png', size: 6, margin: 0, parse_url: true })
       : null,
     [server.uri])
 
   return buffer
     ? h(
-        ['div', { style: { width: 100, padding: 6, marginBottom: 10 }}, [
-          ['div', 'Use your phone to control the scene'],
+        ['div.server-inspector', { style: { display: 'flex', justifyContent: 'flex-end', padding: 24 }}, [
+          ['div', { style: { width: '6rem', textAlign: 'right', paddingRight: 18 }}, 'Use your phone to direct the scene:'],
           ['img', { src: `data:image/png;base64,` + buffer.toString('base64') }]
         ]]
       )
