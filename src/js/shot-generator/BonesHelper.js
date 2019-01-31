14072cf857d6a174528d83e3dc7227c5d4bc5b37
tvvik <vladlazin@gmail.com>
2019-01-31 21:09:43 +0100

    hit mesh size based on vertex distance (projection on plane perpendicular to bone) from bone

--- src/js/shot-generator/BonesHelper.js
+++ src/js/shot-generator/BonesHelper.js
@@ -15,15 +15,179 @@
 
 const THREE = require('three')
 
+const ModelLoader = require('../services/model-loader')
+
 const { LineSegments } = THREE
 const { Matrix4 } = THREE
 const { VertexColors } = THREE
 const { LineBasicMaterial } = THREE
 const { Color } = THREE
 const { Vector3 } = THREE
+const { Vector4 } = THREE
+const { Quaternion } = THREE
 const { BufferGeometry } = THREE
 const { Float32BufferAttribute } = THREE
 const { Object3D } = THREE
+
+const getVertexForBones = ( bufferPositions, bufferSkinIndices, bufferSkinWeights ) => {
+    
+  let bonesInfluenceVertices = []
+  for ( var i = 0; i < bufferSkinIndices.count; i++ ) {
+    let boneIndex = new Vector4()
+    let vertex = new Vector3()
+    let vertWeight = new Vector4()
+
+    vertex.fromBufferAttribute( bufferPositions, i )
+    boneIndex.fromBufferAttribute( bufferSkinIndices, i )
+    vertWeight.fromBufferAttribute( bufferSkinWeights, i )
+    
+    if (vertWeight.x > 0.01) {
+      if (bonesInfluenceVertices[boneIndex.x] ) {
+        bonesInfluenceVertices[boneIndex.x].push( vertex )
+      } else {
+        bonesInfluenceVertices[boneIndex.x] = [vertex]      
+      }
+    }
+
+    if (vertWeight.y > 0.01) {
+      if (bonesInfluenceVertices[boneIndex.y]) {
+        bonesInfluenceVertices[boneIndex.y].push( vertex )
+      } else {
+        bonesInfluenceVertices[boneIndex.y] = [vertex]      
+      }
+    }
+
+    if (vertWeight.z > 0.01) {
+      if (bonesInfluenceVertices[boneIndex.z]) {
+        bonesInfluenceVertices[boneIndex.z].push( vertex )
+      } else {
+        bonesInfluenceVertices[boneIndex.z] = [vertex]      
+      }
+    }
+
+    if (vertWeight.w > 0.01) {
+      if (bonesInfluenceVertices[boneIndex.w]) {
+        bonesInfluenceVertices[boneIndex.w].push( vertex )
+      } else {
+        bonesInfluenceVertices[boneIndex.w] = [vertex]      
+      }
+    }
+
+  }
+  return bonesInfluenceVertices  
+}
+
+let once = true
+
+const calcMedianDistance = (fixedposition, allverts, object, inverdsedMatrix, bone) => {
+  let allDistances = []
+  let median = 0
+  let maxDist = 0
+  let minDist = 10000000;
+  let tempObj = new THREE.Object3D()
+  let plane
+  //if (once) {
+    let test = new THREE.Mesh(
+      new THREE.PlaneGeometry(0.2,0.2),
+      new THREE.MeshBasicMaterial({
+        color: 0x4400ff,
+        depthTest: false,
+        depthWrite: false,
+        transparent: true,
+        opacity: 0.5,
+        flatShading: true,
+        side: THREE.DoubleSide
+      })
+    )
+    test.position.set(fixedposition.x, fixedposition.y, fixedposition.z)
+    test.quaternion.copy(bone.quaternion)
+    test.quaternion.setFromRotationMatrix( bone.matrixWorld )
+    //test.quaternion.multiply(new Quaternion(-Math.sqrt(0.5), 0 , 0, -Math.sqrt(0.5)))
+    test.updateMatrixWorld()
+    test.updateMatrix()
+    plane = new THREE.Plane(fixedposition.clone().applyMatrix4(inverdsedMatrix), 1)
+    plane.applyMatrix4(test.matrix)
+    plane.normalize()
+    //plane.applyMatrix4(test.matrixWorld)
+    //plane.quaternion.copy(test.quaternion)
+    //test.quaternion.multiplyQuaternions()
+
+    let boneProjection = new Vector3()
+    plane.projectPoint(fixedposition, boneProjection)
+    let difference = new Vector3().subVectors(fixedposition, boneProjection)
+    let testPoint = new THREE.Mesh(
+      new THREE.BoxBufferGeometry(0.02,0.02,0.02),
+      new THREE.MeshBasicMaterial({
+        color: 0x4400ff,
+        depthTest: false,
+        depthWrite: false,
+        transparent: true,
+        opacity: 0.5,
+        flatShading: true,
+        side: THREE.DoubleSide
+      })
+    )
+    testPoint.position.set(fixedposition.x, fixedposition.y, fixedposition.z)
+
+    var helper = new THREE.PlaneHelper( plane, 1, 0xffff00 );
+    helper.updateMatrix()
+    if (once) tempObj.add( helper )
+    //if (once) tempObj.add(test)
+    if (once) tempObj.add(testPoint)
+  //}
+
+  for (let vect of allverts)
+  {
+    let vect2 = vect.clone().applyMatrix4(inverdsedMatrix)
+    let vect3 = new Vector3()
+    plane.projectPoint(vect2, vect3)
+    vect3.addVectors(vect3, difference)
+    let distanceFromOriginal = vect3.distanceTo(vect2)
+    if (distanceFromOriginal<0.01)
+    {
+      //console.log('difference from original: ',vect3.distanceTo(vect2))
+      //console.log('dif: ', difference)
+      allDistances.push(fixedposition.distanceTo(vect3))
+
+      if (once) {
+        let test = new THREE.Mesh(
+          new THREE.BoxBufferGeometry(0.01,0.01,0.01),
+          new THREE.MeshBasicMaterial({
+            color: 0xff0044,
+            depthTest: false,
+            depthWrite: false,
+            transparent: true,
+            opacity: 0.5,
+            flatShading: true
+          })
+        )
+        test.position.set(vect3.x, vect3.y, vect3.z)
+        tempObj.add(test)
+      }
+      median += allDistances[allDistances.length-1]
+      if (allDistances[allDistances.length-1] > maxDist) maxDist = allDistances[allDistances.length-1] 
+      if (allDistances[allDistances.length-1] < minDist) minDist = allDistances[allDistances.length-1] 
+    }
+
+   
+  }
+  if (once) {
+    once = false
+  }
+  median = median / allDistances.length
+  return {
+    median: maxDist,
+    object: tempObj
+  }
+}
+
+const getPointInBetweenByPerc = (pointA, pointB, percentage) => {
+  var dir = pointB.clone().sub(pointA);
+  var len = dir.length();
+  dir = dir.normalize().multiplyScalar(len*percentage);
+  return pointA.clone().add(dir);
+}
+
 
 function getBoneList( object ) {
 
@@ -64,6 +228,16 @@ function filter_array(test_array) {
 
 function BonesHelper( object, object3D ) {
   Object3D.call( this )
+  //console.log('object: ', object3D)
+  //ModelLoader.isCustomModel(model)
+  let skeleton = object3D.children[1].skeleton
+  skeleton.pose()
+
+  let skinIndex = object3D.children[1].geometry.attributes.skinIndex
+  let vertexPositions = object3D.children[1].geometry.attributes.position
+  let skinWeights = object3D.children[1].geometry.attributes.skinWeight
+  //console.log('obj: ', vertexPositions)
+
   let bones = getBoneList( object );
   this.cones = []
 
@@ -71,16 +245,16 @@ function BonesHelper( object, object3D ) {
 
   let boneMatrix = new Matrix4()
   let matrixWorldInv = new Matrix4()
-  let posA = new Vector3()
-  let posB = new Vector3()
-  let scaleA = new Vector3()
-  let scaleB = new Vector3()
   let boneCounter = 0
   matrixWorldInv.getInverse( object.matrixWorld )
-
+  
+  let bonesContainingVerts = getVertexForBones(vertexPositions, skinIndex, skinWeights)
   let traversedBones = []
+
   for (var ii = 0; ii< bones.length; ii++) {
     var bone = bones[ii]
+    //console.log('skeleton: ', skeleton.skeleton)
+    //console.log('is it the same bone? : ', skeleton.skeleton.bones[ii] == bones[ii])
     var jj = 0
     let posA = new Vector3()
     let posB = new Vector3()
@@ -89,14 +263,16 @@ function BonesHelper( object, object3D ) {
     let scaleC = new Vector3()
     let spineNumber = 0
 
+    //console.log(object3D.children[1].geometry)
+
     while (bone.children && bone.children[jj] && bone.children[jj].isBone  )
     {
       if (traversedBones.includes(bone)) {
 
         let currentCreated = traversedBones[traversedBones.indexOf(bone)]
-        //console.log('double!: ', currentCreated)
         this.remove(currentCreated)
         this.remove(currentCreated.hitBone)
+        this.remove(currentCreated.helper)
         traversedBones[traversedBones.indexOf(bone)] = bone
       }
       else {
@@ -107,6 +283,20 @@ function BonesHelper( object, object3D ) {
 
       posA.setFromMatrixPosition(boneMatrix.multiplyMatrices(matrixWorldInv, bone.matrixWorld))
       posB.setFromMatrixPosition(boneMatrix.multiplyMatrices(matrixWorldInv, bone.children[jj].matrixWorld))
+      once = ii > 10 && ii < 20 ? true : false
+      //once = true
+      //if (once) console.log('median distance for bone',bone.name)//bone.name)//,': ', distanceToVerts)
+      once = false
+      let distanceToVerts = 0.1 
+      let createdHelper = new Object3D()
+      if (bonesContainingVerts[ii])
+      {
+        let relativePos = getPointInBetweenByPerc(posA, posB, 0.5)
+        let med = calcMedianDistance(relativePos, bonesContainingVerts[ii], this, matrixWorldInv, bone)
+        distanceToVerts = med.median !== 0 ? med.median : 0.1
+        createdHelper = med.object        
+      }
+      //console.log('median distance for bone',bone.name,': ', distanceToVerts)
 
       scaleA.setFromMatrixScale(boneMatrix)
       scaleB.setFromMatrixScale(matrixWorldInv)
@@ -115,13 +305,14 @@ function BonesHelper( object, object3D ) {
       let boneWidth = boneLength > 0.15 ? boneLength : 0.15   //restrict minimum width
       if (boneWidth > 0.35) boneWidth = 0.35  //also maximum..
 
+      let hit_bone_width = distanceToVerts*1.5
       let geometry = new THREE.CylinderBufferGeometry(boneWidth / 25, boneWidth /15 , boneLength - boneWidth/20, 4 )//, heightSegments : Integer, openEnded : Boolean, thetaStart : Float, thetaLength : Float)
-
-      //secondary geometry used for hit testing
+      // secondary geometry used for hit testing
       // if it's mixamo rig all spine bones contain the SPine string, set that to wider
-      let hit_bone_width = ((bone.name.indexOf('Spine')>0)||(bone.name.indexOf('Hips')>0)) ? boneWidth*1.3 : boneWidth / 4
+      //let hit_bone_width = ((bone.name.indexOf('Spine')>0)||(bone.name.indexOf('Hips')>0)) ? boneWidth2 / 4 : boneWidth2 / 4// boneWidth*1.3 : boneWidth / 4
       //let hit_geometry = new THREE.CylinderBufferGeometry(hit_bone_width, hit_bone_width, boneLength, 4)
       //let hit_geometry = new THREE.BoxBufferGeometry(hit_bone_width, hit_bone_width, boneLength)
+      
       let hit_geometry = new THREE.BoxBufferGeometry(hit_bone_width, boneLength, hit_bone_width )
       let hit_material = new THREE.MeshBasicMaterial({
         color: 0x00ff00,
@@ -154,7 +345,7 @@ function BonesHelper( object, object3D ) {
       hitMesh.geometry.applyMatrix(new Matrix4().makeTranslation(0, boneLength/2, 0))
 
       // set visible here to see the hit mesh
-      hitMesh.material.visible = false
+      //hitMesh.material.visible = false
       hitMesh.name = 'hitter_'+bone.name
       hitMesh.userData.type = 'hitter'
 
@@ -165,10 +356,16 @@ function BonesHelper( object, object3D ) {
       this.cones[boneIndex].userData.type = 'bone'
       this.cones[boneIndex].userData.bone = bone.uuid
       this.cones[boneIndex].userData.segment = 0
+
+      //hitMesh.geometry.applyMatrix(new Matrix4().makeScale(4, 1, 4))
       //this.cones[boneCounter].add(s_sphere)
       if (boneLength>0)
       {
         // don't add mesh hitters for hand fingers and shoulders
+
+        // remove custom bone stats for now!
+        
+        /*
         if ( ( bone.name.indexOf('LeftHand')>0 && ( bone.name.charAt(bone.name.indexOf('LeftHand')+8)) !== "" )
           || ( bone.name.indexOf('RightHand')>0 && ( bone.name.charAt(bone.name.indexOf('RightHand')+9)) !== "" )
           || ( bone.name.indexOf('Shoulder')>0 ) )
@@ -207,18 +404,25 @@ function BonesHelper( object, object3D ) {
         }
         if ( bone.name.indexOf('Hips')>0 && ( bone.name.charAt(bone.name.indexOf('Hips')+4)) === "" )
         {
-          hitMesh.geometry.applyMatrix(new Matrix4().makeScale(1.6, 1.8, 1))
+          hitMesh.geometry.applyMatrix(new Matrix4().makeScale(6, 1.8, 5))
           hitMesh.geometry.applyMatrix(new Matrix4().makeTranslation(0, -boneLength, 0))
         }
         if ( (bone.name.indexOf('Spine')>0) )
         {
           //align the spine hit meshes better (the bones are slightly to the back of the mesh)
-          hitMesh.geometry.applyMatrix(new Matrix4().makeScale(1.5, 1, 1))
+          hitMesh.geometry.applyMatrix(new Matrix4().makeScale(6, 1, 5))
 
           hitMesh.geometry.applyMatrix(new Matrix4().makeTranslation(0, 0, boneLength/3 - spineNumber * boneLength/10))
           spineNumber++
         }
+        */
         //removed, adding only when selected
+
+        this.hit_meshes[boneIndex] = ( hitMesh )
+        this.add(hitMesh)
+        this.add(createdHelper)
+
+        bone.helper = createdHelper
         //this.add(this.cones[boneCounter])
         bone.hitBone = hitMesh
         bone.connectedBone = this.cones[boneIndex]
@@ -231,6 +435,7 @@ function BonesHelper( object, object3D ) {
 
     }
   }
+
   let skeletonHelper = new THREE.SkeletonHelper( bones[0] )
   skeletonHelper.material.linewidth = 5
   //this.add(skeletonHelper)
