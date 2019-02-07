2c0caac2d553fffb731481d795fe14a300b699a4
Eric Skogen <audionerd@users.noreply.github.com>
2019-02-07 10:28:41 -0600

    placeholder icons for ortho view controls

--- src/js/shot-generator/Editor.js
+++ src/js/shot-generator/Editor.js
@@ -3031,7 +3031,18 @@ const Editor = connect(
             ['div.column', { style: { width: '300px', background: '#111'} },
               ['div#topdown', { style: { height: '300px' } },
                 // top-down-canvas
-                ['canvas', { key: 'top-down-canvas', tabIndex: 0, ref: smallCanvasRef, id: 'top-down-canvas', style: { width: '100%' }, onPointerDown: onCanvasPointerDown }]
+                ['canvas', { key: 'top-down-canvas', tabIndex: 0, ref: smallCanvasRef, id: 'top-down-canvas', style: { width: '100%' }, onPointerDown: onCanvasPointerDown }],
+                // controls
+                ['div.topdown__controls', [
+                  ['div.row', [
+                    ['a[href=#]', { onClick: preventDefault() }, [[Icon, { src: 'icon-camera-view-autofit' }]]],
+                    ['a[href=#]', { onClick: preventDefault() }, [[Icon, { src: 'icon-camera-view-zoom-in' }]]],
+                    ['a[href=#]', { onClick: preventDefault() }, [[Icon, { src: 'icon-camera-view-zoom-out' }]]],
+                  ]],
+                  ['div.row', [
+                    ['a[href=#]', { onClick: preventDefault() }, [[Icon, { src: 'icon-camera-view-expand' }]]],
+                  ]]
+                ]]
               ],
               ['div#elements', [ElementsPanel, { machineState, transition }]]
             ],
