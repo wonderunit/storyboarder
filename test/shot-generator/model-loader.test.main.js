76309cc143dc5822c5201eaf8da010cbc22a9368
Eric Skogen <audionerd@users.noreply.github.com>
2019-02-15 12:32:38 -0600

    add test for model-loader

---
+++ test/shot-generator/model-loader.test.main.js
@@ -0,0 +1,25 @@
+// npx electron-mocha --renderer test/shot-generator/model-loader.test.main.js
+// npx floss -p test/shot-generator/model-loader.test.main.js
+
+const assert = require('assert')
+
+const ModelLoader = require('../../src/js/services/model-loader')
+
+describe('ModelLoader', () => {
+  describe('#isCustom', () => {
+    it('knows if a model is custom', () => {
+      assert.equal(true, ModelLoader.isCustomModel('models/character/adult-male.glb'))
+      assert.equal(true, ModelLoader.isCustomModel('/path/to/models/character/adult-male.glb'))
+    })
+    it('knows if a model is built-in', () => {
+      assert.equal(false, ModelLoader.isCustomModel('adult-male'))
+    })
+    it('throws an error for unexpected input', () => {
+      assert.throws(
+        // no extension, which would be expected for a built-in model
+        // but with a path, which is only supposed to be for custom models
+        () => ModelLoader.isCustomModel('models/character/adult-male')
+      )
+    })
+  })
+})
