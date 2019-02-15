52c4f485605dbbe3423f50afe1c2949950e1f7be
Eric Skogen <audionerd@users.noreply.github.com>
2019-02-15 15:11:08 -0600

    cache-buster when loading images for thumbnail / posterframe
    fixes #1568

--- src/js/exporters/common.js
+++ src/js/exporters/common.js
@@ -78,6 +78,7 @@ const getCanvasImageSourcesDataForBoard = (board, projectFileAbsolutePath) => {
     let getImageFilePath = (filename) => path.join(path.dirname(projectFileAbsolutePath), 'images', filename)
 
     let loaders = filenames.map(filename => getImage(getImageFilePath(filename)))
+    let loaders = filenames.map(filename => getImage(getImageFilePath(filename + '?' + Math.random())))
 
     Promise.all(loaders).then(images => {
       let canvasImageSourcesData = []
