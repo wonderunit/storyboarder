ae9b234a5b732e22cc46d657d2f2d77f9fab956e
Vlad Lazin <vladlazin@gmail.com>
2019-02-25 17:12:54 +0100

    started volumetrics

---
+++ src/js/shot-generator/Volumetric.js
@@ -0,0 +1,8 @@
+const THREE = require('three')
+window.THREE = window.THREE || THREE
+
+const React = require('react')
+const { useRef, useEffect, useState } = React
+
+const loadingManager = new THREE.LoadingManager()
+const 
