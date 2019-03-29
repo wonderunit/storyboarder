7150fec4379d53571f85974dca07066e797f7249
Eric Skogen <audionerd@users.noreply.github.com>
2019-03-29 14:06:39 -0500

    deleteObjects

--- src/js/shot-generator/Editor.js
+++ src/js/shot-generator/Editor.js
@@ -1047,6 +1047,7 @@ const ListItem = ({ index, style, isScrolling, data }) => {
           selectObject,
           selectObjectToggle,
           updateObject,
+          deleteObjects,
           setActiveCamera
         }
       ]
