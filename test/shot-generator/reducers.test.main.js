9eef326fb8d261b3e0057742387c267304c352a2
Eric Skogen <audionerd@users.noreply.github.com>
2019-04-24 14:34:21 -0500

    add failing test

--- test/shot-generator/reducers.test.main.js
+++ test/shot-generator/reducers.test.main.js
@@ -36,4 +36,46 @@ describe('reducer', () => {
       assert.equal(sceneObjects['6BC46A44-7965-43B5-B290-E3D2B9D15EEE'].displayName, 'Camera 1')
     })
   })
+
+  describe('redux-undo', () => {
+    it('can use groupBy to batch undo-able actions', () => {
+      let json = fs.readFileSync(path.join(__dirname, '..', 'fixtures', 'shot-generator', 'shot-generator.storyboarder'))
+      let data = JSON.parse(json)
+      let payload = data.boards[0].sg.data
+  
+      store.dispatch({ type: 'LOAD_SCENE', payload })
+      let state = store.getState()
+
+      assert(state.sceneObjects.present)
+      assert(Object.keys(state.attachments).length === 0)
+      assert(state.meta.lastSavedHash.length)
+      assert(state.activeCamera.present)
+      assert(Object.keys(state.models).includes('adult-male'))
+
+      store.dispatch({ type: 'CREATE_OBJECT', payload: {
+        id: '0',
+        type: 'object',
+        model: 'box',
+        width: 1,
+        height: 1,
+        depth: 1,
+        x: 0,
+        y: 0,
+        z: 0,
+        rotation: { x: 0, y: 0, z: 0 },
+        visible: true
+        }
+      })
+
+      assert.equal(3, store.getState().sceneObjects.past.length)
+
+      store.dispatch({ type: 'UPDATE_OBJECT', payload: { id: '0', x: 1 } })
+      store.dispatch({ type: 'UPDATE_OBJECT', payload: { id: '0', x: 2 } })
+      store.dispatch({ type: 'UPDATE_OBJECT', payload: { id: '0', x: 3 } })
+      store.dispatch({ type: 'UPDATE_OBJECT', payload: { id: '0', x: 4 } })
+      store.dispatch({ type: 'UPDATE_OBJECT', payload: { id: '0', x: 5 } })
+
+      assert.equal(4, store.getState().sceneObjects.past.length)
+    })
+  })
 })
