3e2386ce192c748d2f0bbc4698827dd3f830cfd8
Eric Skogen <audionerd@users.noreply.github.com>
2019-04-24 14:26:22 -0500

    cleanup reducers test

--- test/shot-generator/reducers.test.main.js
+++ test/shot-generator/reducers.test.main.js
@@ -1,6 +1,7 @@
 /* global describe it  */
 
 // npx floss -p test/shot-generator/reducers.test.main.js
+// npx mocha -w -R min test/shot-generator/reducers.test.main.js
 
 const assert = require('assert')
 const fs = require('fs')
@@ -8,32 +9,31 @@ const path = require('path')
 
 const { createStore } = require('redux')
 
-const r = require('../../src/js/shared/reducers/shot-generator')
+const { reducer, getSceneObjects } = require('../../src/js/shared/reducers/shot-generator')
 
-const store = createStore(r.reducer)
+const store = createStore(reducer)
 
 describe('reducer', () => {
   describe('sceneObjects', () => {
     it('has a displayName when name is undefined', () => {
       let state = store.getState()
-      assert.equal(state.sceneObjects['6BC46A44-7965-43B5-B290-E3D2B9D15EEE'].displayName, 'Camera 1')
+      assert.equal(getSceneObjects(state)['6BC46A44-7965-43B5-B290-E3D2B9D15EEE'].displayName, 'Camera 1')
     })
-
+  
     it('updates displayName of scene objects when a file is loaded', () => {
       let json = fs.readFileSync(path.join(__dirname, '..', 'fixtures', 'shot-generator', 'shot-generator.storyboarder'))
       let data = JSON.parse(json)
-      let payload = data.boards[0].sts.data
-
+      let payload = data.boards[0].sg.data
+  
       store.dispatch({ type: 'LOAD_SCENE', payload })
       let state = store.getState()
-
-      for (let id in state.sceneObjects) {
-        assert(state.sceneObjects[id].displayName != null)
+      let sceneObjects = getSceneObjects(state)
+  
+      for (let id in sceneObjects) {
+        assert(sceneObjects[id].displayName != null)
       }
-
-      assert.equal(state.sceneObjects['6BC46A44-7965-43B5-B290-E3D2B9D15EEE'].displayName, 'Camera 1')
+  
+      assert.equal(sceneObjects['6BC46A44-7965-43B5-B290-E3D2B9D15EEE'].displayName, 'Camera 1')
     })
   })
 })
-
-
