05f17105b7830415aee6c1d8b8060b5efa9c40d4
Eric Skogen <audionerd@users.noreply.github.com>
2019-04-25 15:22:38 -0500

    start connecting skeleton

--- src/js/express-xr/src/components/SGCharacter.js
+++ src/js/express-xr/src/components/SGCharacter.js
@@ -1,6 +1,4 @@
-const { useMemo } = React
-const { useUpdate } = require('react-three-fiber')
-require('../../../vendor/three/examples/js/utils/SkeletonUtils')
+const { useMemo, useState } = React
 
 const materialFactory = () => new THREE.MeshToonMaterial({
   color: 0xffffff,
@@ -13,16 +11,65 @@ const materialFactory = () => new THREE.MeshToonMaterial({
   morphTargets: true
 })
 
-const SGCharacter = ({ id, model, modelData, x, y, z }) => {
+const cloneGltf = (gltf) => {
+  const clone = {
+    animations: gltf.animations,
+    scene: gltf.scene.clone(true)
+  };
+
+  const skinnedMeshes = {};
+
+  gltf.scene.traverse(node => {
+    if (node.isSkinnedMesh) {
+      skinnedMeshes[node.name] = node;
+    }
+  });
+
+  const cloneBones = {};
+  const cloneSkinnedMeshes = {};
+
+  clone.scene.traverse(node => {
+    if (node.isBone) {
+      cloneBones[node.name] = node;
+    }
+
+    if (node.isSkinnedMesh) {
+      cloneSkinnedMeshes[node.name] = node;
+    }
+  });
+
+  for (let name in skinnedMeshes) {
+    const skinnedMesh = skinnedMeshes[name];
+    const skeleton = skinnedMesh.skeleton;
+    const cloneSkinnedMesh = cloneSkinnedMeshes[name];
+
+    const orderedCloneBones = [];
+
+    for (let i = 0; i < skeleton.bones.length; ++i) {
+      const cloneBone = cloneBones[skeleton.bones[i].name];
+      orderedCloneBones.push(cloneBone);
+    }
+
+    cloneSkinnedMesh.bind(
+        new THREE.Skeleton(orderedCloneBones, skeleton.boneInverses),
+        cloneSkinnedMesh.matrixWorld);
+  }
+
+  return clone;
+}
+
+const SGCharacter = ({ id, model, modelData, x, y, z, skeleton }) => {
   const skinnedMesh = useMemo(
     () => {
       if (modelData) {
+        let data = cloneGltf(modelData)
+
         let source = (
-          modelData.scene.children.find(child => child instanceof THREE.SkinnedMesh) ||
-          modelData.scene.children[0].children.find(child => child instanceof THREE.SkinnedMesh)
+          data.scene.children.find(child => child instanceof THREE.SkinnedMesh) ||
+          data.scene.children[0].children.find(child => child instanceof THREE.SkinnedMesh)
         )
 
-        let skinnedMesh =  THREE.SkeletonUtils.clone(source)
+        let skinnedMesh = source
 
         let material = materialFactory()
         if (skinnedMesh.material.map) {
@@ -37,12 +84,45 @@ const SGCharacter = ({ id, model, modelData, x, y, z }) => {
     [modelData]
   )
 
+  // const armature = useMemo(
+  //   () => {
+  //     if (modelData) {
+  //       let armature = modelData.scene
+  //         .children[0]
+  //         .children
+  //         .find(child => child instanceof THREE.Object3D)
+  //         .children
+  //         .find(child => child instanceof THREE.Bone)
+  //       return armature.clone()
+  //     }
+  //   },
+  //   [modelData]
+  // )
+
+  useMemo(() => {
+    if (!skinnedMesh) return
+    if (!Object.keys(skeleton).length) return
+
+    for (let name in skeleton) {
+      let bone = skinnedMesh.skeleton.getBoneByName(name)
+      if (bone) {
+        // console.log(bone.name, { x, y, z } = skeleton[name].rotation)
+        bone.rotation.x = skeleton[name].rotation.x
+        bone.rotation.y = skeleton[name].rotation.y
+        bone.rotation.z = skeleton[name].rotation.z
+      }
+    }
+  }, [skinnedMesh, skeleton])
+
   return skinnedMesh
     ? <group
       userData={{ id }}
       position={[ x, z, y ]}
       >
         <primitive object={skinnedMesh} />
+        {/*
+        <primitive object={armature} />
+        */}
       </group>
     : null
 }
