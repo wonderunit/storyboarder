a2d8ba7d1a23c014aee1204904a3b63a77edc7f9
Eric Skogen <audionerd@users.noreply.github.com>
2019-04-26 12:27:30 -0500

    extract cloneGltf

--- src/js/express-xr/src/components/SGCharacter.js
+++ src/js/express-xr/src/components/SGCharacter.js
@@ -1,5 +1,7 @@
 const { useMemo, useState, useRef, useEffect } = React
 
+const cloneGltf = require('../lib/clone-gltf')
+
 const materialFactory = () => new THREE.MeshToonMaterial({
   color: 0xffffff,
   emissive: 0x0,
@@ -10,53 +12,6 @@ const materialFactory = () => new THREE.MeshToonMaterial({
   morphNormals: true,
   morphTargets: true
 })
-
-const cloneGltf = (gltf) => {
-  const clone = {
-    animations: gltf.animations,
-    scene: gltf.scene.clone(true)
-  };
-
-  const skinnedMeshes = {};
-
-  gltf.scene.traverse(node => {
-    if (node.isSkinnedMesh) {
-      skinnedMeshes[node.name] = node;
-    }
-  });
-
-  const cloneBones = {};
-  const cloneSkinnedMeshes = {};
-
-  clone.scene.traverse(node => {
-    if (node.isBone) {
-      cloneBones[node.name] = node;
-    }
-
-    if (node.isSkinnedMesh) {
-      cloneSkinnedMeshes[node.name] = node;
-    }
-  });
-
-  for (let name in skinnedMeshes) {
-    const skinnedMesh = skinnedMeshes[name];
-    const skeleton = skinnedMesh.skeleton;
-    const cloneSkinnedMesh = cloneSkinnedMeshes[name];
-
-    const orderedCloneBones = [];
-
-    for (let i = 0; i < skeleton.bones.length; ++i) {
-      const cloneBone = cloneBones[skeleton.bones[i].name];
-      orderedCloneBones.push(cloneBone);
-    }
-
-    cloneSkinnedMesh.bind(
-        new THREE.Skeleton(orderedCloneBones, skeleton.boneInverses),
-        cloneSkinnedMesh.matrixWorld);
-  }
-
-  return clone;
-}
 
 const SGCharacter = ({ id, model, modelData, x, y, z, skeleton, ...props }) => {
   const object = useRef(null)
