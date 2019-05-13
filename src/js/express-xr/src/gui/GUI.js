bb3a01fc1040e754af831c82cba4be4a95d9306b
Toseben <arekoskela@gmail.com>
2019-05-14 00:43:09 +0300

    position gui correctly

--- src/js/express-xr/src/gui/GUI.js
+++ src/js/express-xr/src/gui/GUI.js
@@ -57,8 +57,8 @@ const GUI = ({ aspectRatio, guiMode, currentBoard, selectedObject }) => {
       value_text.position.y = -idx * textPadding
       value_text.position.x = 0.06
 
-      children.push(<primitive key={`${idx}_prop`} object={prop_text} />)
-      children.push(<primitive key={`${idx}_value`} object={value_text} />)
+      children.push(<primitive key={`${key}_prop`} object={prop_text} />)
+      children.push(<primitive key={`${key}_value`} object={value_text} />)
 
       idx++
     }
@@ -76,143 +76,145 @@ const GUI = ({ aspectRatio, guiMode, currentBoard, selectedObject }) => {
   const eye_texture = useMemo(() => new THREE.TextureLoader().load('/data/system/xr/eye.png'), [])
 
   return (
-    <group rotation={[(Math.PI / 180) * -45, 0, 0]} userData={{ type: 'gui' }}>
-      {selectedObject && (
-        <group
-          position={[
-            (uiScale * 1.75 * 0.5 + uiScale * 0.5 + (uiScale * 0.5 + uiScale * 0.5) + bWidth * 2) * -1,
-            ((textCount + 1) * textPadding + bWidth * 2) * 0.5 - uiScale * 0.5,
-            0
-          ]}
-        >
+    <group rotation={[(Math.PI / 180) * -30, 0, 0]} userData={{ type: 'gui' }} position={[0, 0.015, -0.005]}>
+      <group rotation={[(Math.PI / 180) * -70, 0, 0]}>
+        {selectedObject && (
+          <group
+            position={[
+              (uiScale * 1.75 * 0.5 + uiScale * 0.5 + (uiScale * 0.5 + uiScale * 0.5) + bWidth * 2) * -1,
+              ((textCount + 1) * textPadding + bWidth * 2) * 0.5 - uiScale * 0.5,
+              0
+            ]}
+          >
+            <GUIElement
+              {...{
+                name: 'properties_ui',
+                width: uiScale * 1.75,
+                height: (textCount + 1) * textPadding + bWidth * 2,
+                radius: bWidth,
+                color: 'black'
+              }}
+            />
+            <group
+              position={[
+                uiScale * 1.75 * -0.5 + bWidth,
+                ((textCount + 1) * textPadding + bWidth * 2) * 0.5 - textPadding * 0.475 - bWidth,
+                0.001
+              ]}
+            >
+              {propTexts}
+            </group>
+          </group>
+        )}
+
+        <group position={[(uiScale * 0.5 + uiScale * 0.5 + bWidth) * -1, 0, 0]}>
           <GUIElement
             {...{
-              name: 'properties_ui',
-              width: uiScale * 1.75,
-              height: (textCount + 1) * textPadding + bWidth * 2,
+              name: 'tools_ui',
+              width: uiScale,
+              height: uiScale,
               radius: bWidth,
               color: 'black'
             }}
           />
-          <group
-            position={[
-              uiScale * 1.75 * -0.5 + bWidth,
-              ((textCount + 1) * textPadding + bWidth * 2) * 0.5 - textPadding * 0.475 - bWidth,
-              0.001
-            ]}
-          >
-            {propTexts}
+        </group>
+
+        <group position={[(uiScale * 0.5 + uiScale * 0.5 + bWidth) * -1, 0, 0.001]} scale={[0.9, 0.9, 0.9]}>
+          <group position={[uiScale * -0.25, uiScale * 0.25, 0]} scale={[0.8, 0.8, 0.8]}>
+            <GUIElement
+              {...{
+                icon: selection_texture,
+                name: 'selection_mode',
+                width: uiScale * 0.5,
+                height: uiScale * 0.5,
+                radius: bWidth,
+                color: guiMode === 'selection' ? 0x6e6e6e : 0x212121
+              }}
+            />
+          </group>
+
+          <group position={[uiScale * 0.25, uiScale * 0.25, 0]} scale={[0.8, 0.8, 0.8]}>
+            <GUIElement
+              {...{
+                icon: duplicate_texture,
+                name: 'duplicate_mode',
+                width: uiScale * 0.5,
+                height: uiScale * 0.5,
+                radius: bWidth,
+                color: guiMode === 'duplicate' ? 0x6e6e6e : 0x212121
+              }}
+            />
+          </group>
+
+          <group position={[uiScale * -0.25, uiScale * -0.25, 0]} scale={[0.8, 0.8, 0.8]}>
+            <GUIElement
+              {...{
+                icon: add_texture,
+                name: 'add_mode',
+                width: uiScale * 0.5,
+                height: uiScale * 0.5,
+                radius: bWidth,
+                color: guiMode === 'add' ? 0x6e6e6e : 0x212121
+              }}
+            />
+          </group>
+
+          <group position={[uiScale * 0.25, uiScale * -0.25, 0]} scale={[0.8, 0.8, 0.8]}>
+            <GUIElement
+              {...{
+                icon: erase_texture,
+                name: 'erase_mode',
+                width: uiScale * 0.5,
+                height: uiScale * 0.5,
+                radius: bWidth,
+                color: guiMode === 'erase' ? 0x6e6e6e : 0x212121
+              }}
+            />
           </group>
         </group>
-      )}
 
-      <group position={[(uiScale * 0.5 + uiScale * 0.5 + bWidth) * -1, 0, 0]}>
-        <GUIElement
-          {...{
-            name: 'tools_ui',
-            width: uiScale,
-            height: uiScale,
-            radius: bWidth,
-            color: 'black'
-          }}
-        />
-      </group>
-
-      <group position={[(uiScale * 0.5 + uiScale * 0.5 + bWidth) * -1, 0, 0.001]} scale={[0.9, 0.9, 0.9]}>
-        <group position={[uiScale * -0.25, uiScale * 0.25, 0]} scale={[0.8, 0.8, 0.8]}>
+        <group position={[uiScale * 1.5 * 0.5 + uiScale * 0.5 + bWidth, 0, 0]}>
           <GUIElement
             {...{
-              icon: selection_texture,
-              name: 'selection_mode',
-              width: uiScale * 0.5,
+              name: 'undo_ui',
+              width: uiScale * 1.5,
               height: uiScale * 0.5,
               radius: bWidth,
-              color: guiMode === 'selection' ? 0x6e6e6e : 0x212121
+              color: 'black'
             }}
           />
         </group>
 
-        <group position={[uiScale * 0.25, uiScale * 0.25, 0]} scale={[0.8, 0.8, 0.8]}>
-          <GUIElement
-            {...{
-              icon: duplicate_texture,
-              name: 'duplicate_mode',
-              width: uiScale * 0.5,
-              height: uiScale * 0.5,
-              radius: bWidth,
-              color: guiMode === 'duplicate' ? 0x6e6e6e : 0x212121
-            }}
-          />
-        </group>
+        <group position={[uiScale * 1.5 * 0.5 + uiScale * 0.5 + bWidth, 0, 0.001]} scale={[1, 1, 1]}>
+          <group position={[uiScale * -0.5, 0, 0]} scale={[-0.8, 0.8, 0.8]}>
+            <GUIElement
+              {...{
+                icon: arrow_texture,
+                name: 'prev_board',
+                width: uiScale * 0.5,
+                height: uiScale * 0.5,
+                radius: bWidth,
+                color: currentBoard === 'prev' ? 0x6e6e6e : 0x212121
+              }}
+            />
+          </group>
 
-        <group position={[uiScale * -0.25, uiScale * -0.25, 0]} scale={[0.8, 0.8, 0.8]}>
-          <GUIElement
-            {...{
-              icon: add_texture,
-              name: 'add_mode',
-              width: uiScale * 0.5,
-              height: uiScale * 0.5,
-              radius: bWidth,
-              color: guiMode === 'add' ? 0x6e6e6e : 0x212121
-            }}
-          />
-        </group>
-
-        <group position={[uiScale * 0.25, uiScale * -0.25, 0]} scale={[0.8, 0.8, 0.8]}>
-          <GUIElement
-            {...{
-              icon: erase_texture,
-              name: 'erase_mode',
-              width: uiScale * 0.5,
-              height: uiScale * 0.5,
-              radius: bWidth,
-              color: guiMode === 'erase' ? 0x6e6e6e : 0x212121
-            }}
-          />
+          <group position={[uiScale * 0.5, 0, 0]} scale={[0.8, 0.8, 0.8]}>
+            <GUIElement
+              {...{
+                icon: arrow_texture,
+                name: 'next_board',
+                width: uiScale * 0.5,
+                height: uiScale * 0.5,
+                radius: bWidth,
+                color: currentBoard === 'next' ? 0x6e6e6e : 0x212121
+              }}
+            />
+          </group>
         </group>
       </group>
 
-      <group position={[uiScale * 1.5 * 0.5 + uiScale * 0.5 + bWidth, 0, 0]}>
-        <GUIElement
-          {...{
-            name: 'undo_ui',
-            width: uiScale * 1.5,
-            height: uiScale * 0.5,
-            radius: bWidth,
-            color: 'black'
-          }}
-        />
-      </group>
-
-      <group position={[uiScale * 1.5 * 0.5 + uiScale * 0.5 + bWidth, 0, 0.001]} scale={[1, 1, 1]}>
-        <group position={[uiScale * -0.5, 0, 0]} scale={[-0.8, 0.8, 0.8]}>
-          <GUIElement
-            {...{
-              icon: arrow_texture,
-              name: 'prev_board',
-              width: uiScale * 0.5,
-              height: uiScale * 0.5,
-              radius: bWidth,
-              color: currentBoard === 'prev' ? 0x6e6e6e : 0x212121
-            }}
-          />
-        </group>
-
-        <group position={[uiScale * 0.5, 0, 0]} scale={[0.8, 0.8, 0.8]}>
-          <GUIElement
-            {...{
-              icon: arrow_texture,
-              name: 'next_board',
-              width: uiScale * 0.5,
-              height: uiScale * 0.5,
-              radius: bWidth,
-              color: currentBoard === 'next' ? 0x6e6e6e : 0x212121
-            }}
-          />
-        </group>
-      </group>
-
-      <group position={[0, 0.25, 0]}>
+      <group position={[0, 0.05, -0.075]} rotation={[(Math.PI / 180) * -20, 0, 0]}>
         <SGVirtualCamera {...{ aspectRatio, camOffset: new THREE.Vector3(0, -0.25, 0), ...camSettings }} />
 
         <group
