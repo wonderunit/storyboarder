7cadf895e3172e3ab63817c2dd812a0211a33c5b
Eric Skogen <audionerd@users.noreply.github.com>
2019-05-14 15:52:23 -0500

    swap

--- src/js/window/components/ShotGeneratorPanel.js
+++ src/js/window/components/ShotGeneratorPanel.js
@@ -4,10 +4,10 @@ const placeholderPath = `./img/shot-generator/placeholder-thumbnails`
 
 const placeholder = aspectRatio =>
  ({
-   0.5625: 'shot-generator-bg-16x9',
+   0.5625: 'shot-generator-bg-9x16',
    1: 'shot-generator-bg-1x1',
    1.3333333333333333: 'shot-generator-bg-4x3',
-   1.7777777777777777: 'shot-generator-bg-9x16',
+   1.7777777777777777: 'shot-generator-bg-16x9',
    1.85: 'shot-generator-bg-1.85x1',
    2.39: 'shot-generator-bg-2.39',
    2: 'shot-generator-bg-2x1'
