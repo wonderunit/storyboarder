20477d4e581fc4011710fb14bb74798c3529598e
Eric Skogen <audionerd@users.noreply.github.com>
2019-05-14 15:20:26 -0500

    render placeholder images for new shot-generator thumbnails

--- src/js/window/components/ShotGeneratorPanel.js
+++ src/js/window/components/ShotGeneratorPanel.js
@@ -1,5 +1,18 @@
 const h = require('../../utils/h')
 
+const placeholderPath = `./img/shot-generator/placeholder-thumbnails`
+
+const placeholder = aspectRatio =>
+ ({
+   0.5625: 'shot-generator-bg-16x9',
+   1: 'shot-generator-bg-1x1',
+   1.3333333333333333: 'shot-generator-bg-4x3',
+   1.7777777777777777: 'shot-generator-bg-9x16',
+   1.85: 'shot-generator-bg-1.85x1',
+   2.39: 'shot-generator-bg-2.39',
+   2: 'shot-generator-bg-2x1'
+ }[aspectRatio])
+
 const ShotGeneratorPanel = ({ thumbnail, onClick, aspectRatio }) => {
   return h(
     ['div', [
@@ -17,10 +30,16 @@ const ShotGeneratorPanel = ({ thumbnail, onClick, aspectRatio }) => {
               width: '100%',
               style: {
                 width: '100%',
-                height: 0,
-                paddingTop: `${1 / aspectRatio * 100}%`
               }
-            }]
+            }, [
+              [
+                'img',
+                {
+                  width: '100%',
+                  src: `${placeholderPath}/${placeholder(aspectRatio)}.png`
+                }
+              ]
+            ]]
         ]]
       ]]
     ]]
