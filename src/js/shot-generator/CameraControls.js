e0646ffab692a42a7906109a86e3eb519d0e13ca
Eric Skogen <audionerd@users.noreply.github.com>
2019-05-20 14:10:18 -0500

    better tracking for undo-able groups of key presses

--- src/js/shot-generator/CameraControls.js
+++ src/js/shot-generator/CameraControls.js
@@ -97,6 +97,8 @@ class CameraControls {
     // Ignore Cmd + R (reload) and Cmd + D (duplicate)
     if (event.metaKey) return
 
+    this.addKey(event.keyCode)
+
     switch ( event.keyCode ) {
       case 38: /*up*/
       case 87: /*W*/
@@ -111,7 +113,6 @@ class CameraControls {
         if ( this.moveForward || this.moveBackward || this.moveLeft || this.moveRight || this.moveUp || this.moveDown) {
         } else {
           this.movementSpeed = .0001
-          this.addKey(event.keyCode)
         }
         break
       case 16:
@@ -134,18 +135,20 @@ class CameraControls {
   }
 
   onKeyUp ( event ) {
+    this.removeKey(event.keyCode)
+
     switch ( event.keyCode ) {
       case 38: /*up*/
-      case 87: /*W*/ this.moveForward = false; this.removeKey(event.keyCode); break;
+      case 87: /*W*/ this.moveForward = false; break;
       case 37: /*left*/
-      case 65: /*A*/ this.moveLeft = false; this.removeKey(event.keyCode); break;
+      case 65: /*A*/ this.moveLeft = false; break;
       case 40: /*down*/
-      case 83: /*S*/ this.moveBackward = false; this.removeKey(event.keyCode); break;
+      case 83: /*S*/ this.moveBackward = false; break;
       case 39: /*right*/
-      case 68: /*D*/ this.moveRight = false; this.removeKey(event.keyCode); break;
-      case 82: /*R*/ this.moveUp = false; this.removeKey(event.keyCode); break;
-      case 70: /*F*/ this.moveDown = false; this.removeKey(event.keyCode); break;
-      case 16: /* shift */ this.runMode = false; this.removeKey(event.keyCode); break;
+      case 68: /*D*/ this.moveRight = false; break;
+      case 82: /*R*/ this.moveUp = false; break;
+      case 70: /*F*/ this.moveDown = false; break;
+      case 16: /* shift */ this.runMode = false; break;
     }
   }
 
