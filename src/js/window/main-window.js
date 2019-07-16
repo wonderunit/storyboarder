e29880abf6ded114050c95cf5e9d8368415519a0
Eric Skogen <audionerd@users.noreply.github.com>
2019-07-16 14:01:35 -0500

    svg use

--- src/js/window/main-window.js
+++ src/js/window/main-window.js
@@ -6506,7 +6506,9 @@ const TimelineModeControlView = ({ mode = 'sequence', show = false }) => {
         className: mode === 'sequence' ? 'selected' : null,
         onPointerUp: onTimelineSelect
       },
-        ['svg', { className: 'icon', href: './img/timeline-boards.svg' }],
+        ['svg', { className: 'icon' },
+          ['use', { xlinkHref: './img/timeline-boards.svg' }]
+        ],
         ['span', 'Boards']
       ],
       ['div.spacer'],
@@ -6514,7 +6516,9 @@ const TimelineModeControlView = ({ mode = 'sequence', show = false }) => {
         className: mode !== 'sequence' ? 'selected' : null,
         onPointerUp: onBoardsSelect
       },
-      ['svg', { className: 'icon', href: './img/timeline-timeline.svg' }],
+        ['svg', { className: 'icon' },
+          ['use', { xlinkHref: './img/timeline-timeline.svg' }]
+        ],
         ['span', 'Timeline']
       ]
     ]
