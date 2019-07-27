wonderunit/storyboarder

commit d0cbd622777c765dbf67d9376ca10f79d455109c

change from using THREE ID to Shot Generator ID for selections

src/js/express-xr/src/gui/GUI.js
@@ -16,6 +16,7 @@ const bWidth = 0.0125
 
 const GUI = ({
   rStatsRef,
+  worldScaleGroupRef,
   aspectRatio,
   models,
   presets,
@@ -62,10 +63,20 @@ const GUI = ({
   //   return textCreator.create(`${camSettings.fov}mm`, { centerText: 'custom' })
   // }, [])
 
+  // first look for matching id under world group, if not found look under vrControllers
+  let object = worldScaleGroupRef.current
+    ? worldScaleGroupRef.current.children.find(child => child.userData.id === selectedObject)
+    : undefined
+  if (object === undefined) {
+    vrControllers.forEach(controller => {
+      const match = controller.children.find(child => child.userData.id === selectedObject)
+      if (match) object = match
+    })
+    if (object === undefined) object = false
+  }
+
   const updateGeometry = (id, prop, value) => {
     if (id && prop) {
-      let object = scene.getObjectById(id)
-
       if (prop === 'guiFOV') {
         const guiCam = scene.getObjectByName('guiCam')
         guiCam.dispatchEvent({ type: 'updateFOV', fov: value })
@@ -199,7 +210,6 @@ const GUI = ({
       scene.remove(obj)
     })
 
-    const object = scene.getObjectById(selectedObject)
     if (!object) return []
 
     const parent = findParent(object)
@@ -380,14 +390,13 @@ const GUI = ({
   ]
 
   const invertGUI = flipHand ? -1 : 1
-  const object = scene.getObjectById(selectedObject)
   const fpsMeter = useMemo(() => textCreator.create(fps.toString(), { color: 0xff0000, scale: 0.475, centerText: false }), [fps])
 
   return (
     <group rotation={[(Math.PI / 180) * -30, 0, 0]} userData={{ type: 'gui' }} position={[0, 0.015, -0.005]}>
       <group rotation={[(Math.PI / 180) * -70, 0, 0]}>
         <group name="selector_container">
-          {selectedObject && object.userData.type === 'character' && guiSelector === 'pose' && (
+          {object && object.userData.type === 'character' && guiSelector === 'pose' && (
             <group
               position={[
                 ((uiScale * 2 + bWidth) * 0.5 +
@@ -460,7 +469,7 @@ const GUI = ({
             </group>
           )}
 
-          {selectedObject && object.userData.type === 'character' && guiSelector === 'character' && (
+          {object && object.userData.type === 'character' && guiSelector === 'character' && (
             <group
               position={[
                 ((uiScale * 2 + bWidth) * 0.5 +
@@ -533,7 +542,7 @@ const GUI = ({
             </group>
           )}
 
-          {selectedObject && object.userData.type === 'object' && guiSelector === 'object' && (
+          {object && object.userData.type === 'object' && guiSelector === 'object' && (
             <group
               position={[
                 ((uiScale * 2 + bWidth) * 0.5 +
@@ -608,7 +617,7 @@ const GUI = ({
         </group>
 
         <group name="properties_container">
-          {selectedObject && textCount && (
+          {object && textCount && (
             <group
               position={[
                 (uiScale * 2.75 * 0.5 + uiScale * 0.5 + (uiScale * 0.5 + uiScale * 0.5) + bWidth * 2) * -1 * invertGUI,

src/js/express-xr/src/SceneManagerXR.js
@@ -144,7 +144,7 @@ const SceneContent = ({
   const [selectorOffset, setSelectorOffset] = useState(0)
 
   const worldScaleRef = useRef(0.1)
-  const worldScaleGroupRef = useRef([])
+  const worldScaleGroupRef = useRef(null)
   const moveCamRef = useRef(null)
   const rotateCamRef = useRef(null)
   const intersectArray = useRef([])
@@ -302,19 +302,19 @@ const SceneContent = ({
       if (intersection.object.name.includes('selector-pose')) {
         const posePresetId = intersection.object.name.split('_')[1]
         const skeleton = presets.poses[posePresetId].state.skeleton
-        const object = scene.getObjectById(selectedObject)
+        const object = worldScaleGroupRef.current.children.find(child => child.userData.id === selectedObject)
         updateObject(object.userData.id, { posePresetId, skeleton })
       }
 
       if (intersection.object.name.includes('selector-object')) {
         const model = intersection.object.name.split('_')[1]
-        const object = scene.getObjectById(selectedObject)
+        const object = worldScaleGroupRef.current.children.find(child => child.userData.id === selectedObject)
         updateObject(object.userData.id, { model })
       }
 
       if (intersection.object.name.includes('selector-character')) {
         const model = intersection.object.name.split('_')[1]
-        const object = scene.getObjectById(selectedObject)
+        const object = worldScaleGroupRef.current.children.find(child => child.userData.id === selectedObject)
         updateObject(object.userData.id, { model })
       }
 
@@ -420,7 +420,7 @@ const SceneContent = ({
       }
 
       let object = findParent(intersection.object)
-      const { id } = object
+      const { id } = object.userData
       // is this probably NOT a scene object?
       // (used to exclude environment meshes for example)
       if (object.userData.id == null) {
@@ -428,7 +428,7 @@ const SceneContent = ({
       }
 
       setSelectedObject(id)
-      selectedObjRef.current = scene.getObjectById(id)
+      selectedObjRef.current = object
       setHideArray(createHideArray(scene))
       setGuiMode('selection')
 
@@ -512,7 +512,7 @@ const SceneContent = ({
           setGuiMode('selection')
         }, 250)
         break
-      case 'duplicate':
+      case 'duplicate':        
         if (!selectedObjRef.current) return
         setGuiMode(mode)
 
@@ -521,7 +521,7 @@ const SceneContent = ({
 
         setTimeout(() => {
           const match = worldScaleGroupRef.current.children.find(child => child.userData.id === id)
-          setSelectedObject(match.id)
+          setSelectedObject(match.userData.id)
           selectedObjRef.current = match
           setHideArray(createHideArray(scene))
           setGuiMode('selection')
@@ -873,9 +873,9 @@ const SceneContent = ({
       if (intersection.object.userData.type === 'bone') return
 
       let object = findParent(intersection.object)
-      const { id } = object
+      const { id } = object.userData
       setSelectedObject(id)
-      selectedObjRef.current = scene.getObjectById(id)
+      selectedObjRef.current = object
       setHideArray(createHideArray(scene))
       setGuiMode('selection')
     } else {
@@ -1202,7 +1202,7 @@ const SceneContent = ({
         return (
           <primitive key={n} object={object}>
             {handedness === hand && (
-              <GUI {...{ rStatsRef, models, presets, aspectRatio, guiMode, addMode, currentBoard, selectedObject, hideArray, virtualCamVisible, flipHand, selectorOffset, guiSelector, helpToggle, helpSlide, guiCamFOV, vrControllers }} />
+              <GUI {...{ rStatsRef, worldScaleGroupRef, models, presets, aspectRatio, guiMode, addMode, currentBoard, selectedObject, hideArray, virtualCamVisible, flipHand, selectorOffset, guiSelector, helpToggle, helpSlide, guiCamFOV, vrControllers }} />
             )}
             <SGController
               {...{ flipModel, modelData: getModelData(controllerObjectSettings), ...controllerObjectSettings }}
@@ -1213,11 +1213,11 @@ const SceneContent = ({
     </group>
   )
 
-  const selectedObject3d = scene.getObjectById(selectedObject)
+  const selectedObj3d = worldScaleGroupRef.current ? worldScaleGroupRef.current.children.find(child => child.userData.id === selectedObject) : undefined
 
   let sceneObjectComponents = Object.values(sceneObjects)
     .map((sceneObject, i) => {
-      const isSelected = selectedObject3d && selectedObject3d.userData.id === sceneObject.id
+      const isSelected = selectedObj3d && selectedObj3d.userData.id === sceneObject.id
         ? true
         : false
 

src/js/express-xr/src/components/SGVirtualCamera.js
@@ -33,7 +33,9 @@ const SGVirtualCamera = ({ i, aspectRatio, selectedObject, hideArray, virtualCam
   }, [modelData])
 
   const { gl, scene } = useThree()
-  const selectedObj = findParent(scene.getObjectById(selectedObject))
+  const worldScaleGroup = scene.children.find(child => child.userData.type === 'world-scale')
+  const selectedObj = worldScaleGroup ? findParent(worldScaleGroup.children.find(child => child.userData.id === selectedObject)) : undefined
+
   isSelected.current = selectedObj && selectedObj.userData.id === props.id
 
   const ref = useUpdate(

commit 43598c0a2637cf2de3fede2974e05a7b29380ae7

fix character height slider bug caused by d0cbd622777c765dbf67d9376ca10f79d455109c

src/js/express-xr/src/gui/GUI.js
@@ -89,13 +89,13 @@ const GUI = ({
           break
         case 'height':
           if (object.userData.name === 'character-container') {
-            object = object.children[0]
-            if (object.userData.modelSettings.height) {
-              let originalHeight = object.userData.originalHeight
+            const character = object.children[0]
+            if (character.userData.modelSettings.height) {
+              let originalHeight = character.userData.originalHeight
               let scale = value / originalHeight
-              object.scale.set(scale, scale, scale)
+              character.scale.set(scale, scale, scale)
             } else {
-              object.scale.setScalar(value)
+              character.scale.setScalar(value)
             }
           } else {
             object.scale.y = value