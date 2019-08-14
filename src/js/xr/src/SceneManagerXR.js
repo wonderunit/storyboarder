8d810a5775a9acbfec5a7dbee11906bba507aa65
Eric Skogen <audionerd@users.noreply.github.com>
2019-08-13 20:18:00 -0500

    camera placement

--- src/js/xr/src/SceneManagerXR.js
+++ src/js/xr/src/SceneManagerXR.js
@@ -1,19 +1,63 @@
 const THREE = require('three')
 window.THREE = window.THREE || THREE
-const { Canvas, useThree, useRender } = require('react-three-fiber')
+const { Canvas, useThree } = require('react-three-fiber')
 
-const { connect } = require('react-redux')
+const { connect, useStore, Provider } = require('react-redux')
 const React = require('react')
-const { useMemo } = React
+const { useMemo, useState } = React
 
 const { WEBVR } = require('three/examples/jsm/vr/WebVR')
 
-const SceneContent = () => {
+const {
+  getSceneObjects,
+  getActiveCamera
+} = require('../../shared/reducers/shot-generator')
+
+const SceneContent = connect(
+  state => ({
+    sceneObjects: getSceneObjects(state),
+    activeCamera: getActiveCamera(state)
+  })
+)(
+({
+  sceneObjects,
+  activeCamera
+}) => {
+  const { scene, camera } = useThree()
+
+  const [teleportPos, setTeleportPos] = useState(null)
+  const [teleportRot, setTeleportRot] = useState(null)
+
+  // initialize behind the camera, on the floor
+  useMemo(() => {
+    let { x, y, rotation } = sceneObjects[activeCamera]
+
+    let behindCam = {
+      x: Math.sin(rotation),
+      y: Math.cos(rotation)
+    }
+
+    setTeleportPos(new THREE.Vector3(x + behindCam.x, 0, y + behindCam.y))
+    setTeleportRot(new THREE.Euler(0, rotation, 0))
+  }, [])
+
   return (
     <>
+      <group
+        position={teleportPos}
+        rotation={teleportRot}
+      >
+        <primitive object={camera} />
+      </group>
+
+      <ambientLight color={0xffffff} intensity={1} />
+      <mesh position={[0, 0, 0]} rotation={[0, 0, 0]}>
+        <sphereGeometry attach="geometry" args={[1, 16, 16]} />
+        <meshStandardMaterial attach="material" color="indianred" transparent />
+      </mesh>
     </>
   )
-}
+})
 
 const XRStartButton = ({ }) => {
   const { gl } = useThree()
@@ -37,18 +81,22 @@ const SceneManagerXR = connect(
   ({
     //
   }) => {
-    const loaded = false
+    const store = useStore()
+
+    const loaded = true
 
     return (
       <>
         {
           !loaded && <div className='loading-button'>LOADING …</div>
         }
-        <Canvas vr style={{ visibility: 'hidden' }}>
-          {
-            loaded && <XRStartButton />
-          }
-          <SceneContent />
+        <Canvas vr>
+          <Provider store={store}>
+            {
+              loaded && <XRStartButton />
+            }
+            <SceneContent />
+          </Provider>
         </Canvas>
         <div className="scene-overlay"></div>
       </>
