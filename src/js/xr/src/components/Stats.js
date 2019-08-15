0aa58905a50f6be22020e25983aabea14cc0eac9
Eric Skogen <audionerd@users.noreply.github.com>
2019-08-15 15:30:28 -0500

    comment

--- src/js/xr/src/components/Stats.js
+++ src/js/xr/src/components/Stats.js
@@ -42,6 +42,13 @@ const Stats = ({ rStats, position }) => {
     () => setFps(Math.round(parseFloat(rStats('fps').value()))),
     250)
 
+  // gl.info.render.calls
+  // reports the amount of draw calls for a single render call.
+  // if you have multiple calls of WebGLRenderer.render() in order to produce a single frame,
+  // you need to set renderer.info.autoReset to false
+  // and call renderer.info.reset() manually at the end of your frame.
+  // via https://discourse.threejs.org/t/where-can-i-see-the-number-of-draw-calls-per-frame/4311/3
+
   let label = `F ${fps}\nC ${gl.info.render.calls}\nT ${gl.info.render.triangles}`
 
   return (
