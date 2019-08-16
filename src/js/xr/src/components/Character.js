be18e44d711720fcde00352b157f127256671fc2
Eric Skogen <audionerd@users.noreply.github.com>
2019-08-16 11:13:40 -0500

    Character: use SkinnedMesh, skeleton, position

--- src/js/xr/src/components/Character.js
+++ src/js/xr/src/components/Character.js
@@ -1,30 +1,48 @@
 const THREE = require('three')
 const { useMemo } = React = require('react')
 
-const useModel = require('../hooks/use-model')
+const { unstable_createResource } = require('../../vendor/react-cache')
+const { GLTFLoader } = require('three/examples/jsm/loaders/GLTFLoader')
+
+const resource = unstable_createResource(
+  file => new Promise(async res => new GLTFLoader().load(file, res))
+)
 
 const Character = ({ sceneObject }) => {
   // TODO detect user models, e.g.: `/data/user/characters/${filename}`
-  const [geometries] = useModel(`/data/system/dummies/gltf/${sceneObject.model}-lod.glb`)
+  const filepath = useMemo(
+    () => `/data/system/dummies/gltf/${sceneObject.model}-lod.glb`,
+    [sceneObject.model]
+  )
 
-  const material = useMemo(() => new THREE.MeshBasicMaterial())
+  const { scene } = resource.read(filepath)
 
-  const lod = useMemo(
+  const [skeleton, lod] = useMemo(
     () => {
       let lod = new THREE.LOD()
-      for (let i = 1, d = 0; i < geometries.length; i++, d++) {
-        let geometry = geometries[i]
-        let mesh = new THREE.Mesh(geometry, material)
-        // console.log('adding mesh index', i, 'at distance', d * 2, 'm', '#', geometry.index.count)
+
+      let meshes = scene.children.filter(child => child.isSkinnedMesh)
+
+      for (let i = 1, d = 0; i < meshes.length; i++, d++) {
+        let mesh = meshes[i] // shared reference to the mesh in the cache
         lod.addLevel(mesh, d * 2)
       }
-      return lod
+
+      let skeleton = lod.children[0].skeleton
+      skeleton.pose()
+
+      return [skeleton, lod]
     },
-    [geometries]
+    [scene]
+  )
+
+  const position = useMemo(
+    () => [sceneObject.x, sceneObject.z, sceneObject.y],
+    [sceneObject.x, sceneObject.y, sceneObject.z]
   )
 
   return lod
-    ? <primitive object={lod} />
+    ? <primitive object={lod} position={position} />
     : null
 }
 
