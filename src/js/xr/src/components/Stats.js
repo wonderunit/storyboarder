f0899740beb2b200e0425eb3282b274bda3241fc
Eric Skogen <audionerd@users.noreply.github.com>
2019-08-20 18:37:40 -0500

    extract SimpleText

--- src/js/xr/src/components/Stats.js
+++ src/js/xr/src/components/Stats.js
@@ -1,37 +1,9 @@
-const { useMemo, useRef, useState } = React = require('react')
+const { useState } = React = require('react')
 const { useThree } = require('react-three-fiber')
 
 const useInterval = require('../hooks/use-interval')
 
-const SDFText = require('datguivr/modules/datguivr/sdftext')
-const textCreator = SDFText.creator()
-
-const SimpleText = ({
-  label,
-  textProps = {},
-  ...props
-}) => {
-  const group = useRef(null)
-
-  useMemo(() => {
-    if (group.current) {
-      // changed in dataguivr 0.1.6
-      group.current.update(label.toString())
-    } else {
-      group.current = textCreator.create(
-        label.toString(),
-        {
-          color: 0xffffff,
-          scale: 1,
-          centerText: false,
-          ...textProps
-        }
-      )
-    }
-  }, [label])
-
-  return <primitive {...props} object={group.current} />
-}
+const SimpleText = require('../components/SimpleText')
 
 const Stats = ({ rStats, position }) => {
   const { gl } = useThree()
