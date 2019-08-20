0288848a8c4e49dae76e739a7277cd008145e03e
Vlad Stepura <vlad.stepura3d@gmail.com>
2019-08-20 12:12:35 +0300

    Added CameraHelper to GPUPicker to help figure out camera position

--- src/js/shot-generator/GPUPickers/GPUPicker.js
+++ src/js/shot-generator/GPUPickers/GPUPicker.js
@@ -7,6 +7,7 @@ class GPUPicker
         this.renderer = renderer;
         this.camera = new THREE.PerspectiveCamera(0.001, renderer.domElement.width / renderer.domElement.height, 0.1, 1000 );
         this.controller = new XRGPUPickerController();
+        this.cameraHelper = null;
     }   
 
     setupScene(objects)
@@ -29,6 +30,18 @@ class GPUPicker
         }
         this.camera.updateMatrixWorld(true);
         return this.controller.pick(this.camera, this.renderer);
+    }
+
+    // Creates a CameraHeleper which needed to be added to a scene in order to help to figure out
+    // where current position of the GPUPicker camera
+    getCameraHelper()
+    {
+        if(this.cameraHelper)
+        {
+            return this.cameraHelper;
+        }
+        this.cameraHelper = new THREE.CameraHelper(this.camera);
+        return this.cameraHelper;
     }
 }
 module.exports = GPUPicker;
