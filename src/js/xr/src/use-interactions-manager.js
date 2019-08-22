69700214e3f9344bf3b1ce0ed0fbfbd79b755a85
Eric Skogen <audionerd@users.noreply.github.com>
2019-08-22 14:09:39 -0500

    comment

--- src/js/xr/src/use-interactions-manager.js
+++ src/js/xr/src/use-interactions-manager.js
@@ -137,7 +137,10 @@ const useInteractionsManager = ({
 
     let match = null
     let intersection = null
-    let list = scene.__interaction//.filter(object3d => object3d.userData.type != 'character')
+
+    // let list = scene.__interaction.filter(object3d => object3d.userData.type != 'character')
+    // DEBUG include all interactables so we can test Character
+    let list = scene.__interaction
 
     // find the positioned controller based on the data controller's gamepad array index
     let positionedObject = gl.vr.getController(event.target.gamepad.index)
