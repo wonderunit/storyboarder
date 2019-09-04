12f041bde7a88d1f7a392bb6c5e9e4fe264a5f66
Eric Skogen <audionerd@users.noreply.github.com>
2019-09-04 11:59:23 -0500

    comment

--- test/views/xr-ui/index.js
+++ test/views/xr-ui/index.js
@@ -1,7 +1,7 @@
 // mkdir -p test/views/xr-ui/dist/data/system/xr/ui
 // cp src/data/shot-generator/xr/ui/controls.glb test/views/xr-ui/dist/data/system/xr/ui/controls.glb
 // cp test/fixtures/xr/xr.storyboarder test/views/xr-ui/dist
-// parcel serve --no-hmr test/views/xr-ui/index.html -d test/views/xr-ui/dist -p 9966
+// npx parcel serve --no-hmr test/views/xr-ui/index.html -d test/views/xr-ui/dist -p 9966
 
 const THREE = require('three')
 window.THREE = window.THREE || THREE
