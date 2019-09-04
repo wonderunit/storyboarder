5fd470aff7b6d491b23eab6d57ee6a24a9be0a87
Eric Skogen <audionerd@users.noreply.github.com>
2019-09-04 14:22:35 -0500

    add useImageBitmapLoader

---
+++ src/js/xr/src/hooks/use-imagebitmap-loader.js
@@ -0,0 +1,10 @@
+const THREE = require('three')
+const { unstable_createResource } = require('../../vendor/react-cache')
+
+const resource = unstable_createResource(
+  file => new Promise(async res => new THREE.ImageBitmapLoader().load(file, res))
+)
+
+const useImageBitmapLoader = filepath => resource.read(filepath)
+
+module.exports = useImageBitmapLoader
