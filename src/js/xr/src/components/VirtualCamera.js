5a67003308f61072754fac5db835fc286e1ab3f5
Eric Skogen <audionerd@users.noreply.github.com>
2019-09-06 02:59:15 -0500

    cleanup

--- src/js/xr/src/components/VirtualCamera.js
+++ src/js/xr/src/components/VirtualCamera.js
@@ -1,8 +1,8 @@
 const THREE = require('three')
 const { useMemo, useRef } = React = require('react')
 const useGltf = require('../hooks/use-gltf')
-const { useRender, useThree} = require('react-three-fiber')
-require("../three/GPUPickers/utils/Object3dExtension");
+const { useRender, useThree } = require('react-three-fiber')
+require('../three/GPUPickers/utils/Object3dExtension')
 
 const materialFactory = () => new THREE.MeshLambertMaterial({
   color: 0xcccccc,
@@ -11,9 +11,9 @@ const materialFactory = () => new THREE.MeshLambertMaterial({
 })
 
 const meshFactory = source => {
-  let mesh = source.clone()
+  const mesh = source.clone()
 
-  let material = materialFactory()
+  const material = materialFactory()
 
   if (mesh.material.map) {
     material.map = mesh.material.map
@@ -25,150 +25,136 @@ const meshFactory = source => {
 }
 
 const VirtualCamera = React.memo(({ aspectRatio, sceneObject, isSelected, objectsToRender }) => {
+  const filepath = useMemo(
+    () => `/data/system/objects/camera.glb`,
+    [sceneObject]
+  )
+  const { gl, scene } = useThree()
+  const virtualCamera = useRef(null)
+  const gltf = useGltf(filepath)
+  const ref = useRef(null)
+  const renderTarget = useRef(null)
+  const size = 1 / 3
+  const resolution = 512
+  const previousTime = useRef([null])
 
-    const filepath = useMemo(
-      () => `/data/system/objects/camera.glb`,
-      [sceneObject]
-    )
-    const { gl, scene } = useThree()
-    const virtualCamera = useRef(null);
-    const gltf = useGltf(filepath)
-    const ref = useRef(null);
-    const renderTarget = useRef(null);
-    const size = 1 / 3
-    const resolution = 512;
-    const previousTime = useRef([null])
+  const renderCamera = () => {
+    if (virtualCamera.current && renderTarget.current) {
+      gl.vr.enabled = false
+      scene.autoUpdate = false
+      gl.setRenderTarget(renderTarget.current)
+      gl.render(scene, virtualCamera.current)
+      gl.setRenderTarget(null)
+      scene.autoUpdate = true
 
+      gl.vr.enabled = true
+    }
+  }
 
-    const renderCamera = () => {
-      if (virtualCamera.current && renderTarget.current) {
-        gl.vr.enabled = false
-        scene.autoUpdate = false;
-        gl.setRenderTarget(renderTarget.current)
-        gl.render(scene, virtualCamera.current)
-        gl.setRenderTarget(null)
-        scene.autoUpdate = true;
-        
-        gl.vr.enabled = true
-      }
+  useMemo(() => {
+    if (isSelected) {
+      renderCamera()
+    } else {
+
+    }
+  }, [ref.current, isSelected])
+
+  useRender(() => {
+    if (!previousTime.current) previousTime.current = 0
+
+    const currentTime = Date.now()
+    const delta = currentTime - previousTime.current
+
+    if (delta > 500) {
+      previousTime.current = currentTime
+    } else {
+      if ((!isSelected && !sceneObject.isClose)) return
     }
 
-    useMemo(() => {
-      if (isSelected) {
-        renderCamera();
-      } else {
+    renderCamera()
+  }, false, [isSelected])
 
-      }
-    }, [ref.current, isSelected])
+  useMemo(() => {
+    for (let i = 0, n = objectsToRender.length; i < n; i++) {
+      objectsToRender[i].traverse(object => object.layers.enable(1))
+    }
+  }, [objectsToRender])
 
+  useMemo(() => {
+    if (virtualCamera.current) {
+      virtualCamera.current.layers.set(1)
+    }
+  }, [virtualCamera.current])
 
-    useRender(() => {
-      if (!previousTime.current) previousTime.current = 0
-  
-      const currentTime = Date.now()
-      const delta = currentTime - previousTime.current
-  
-      if (delta > 500) {
-        previousTime.current = currentTime
-      } else {
-        if ((!isSelected && !sceneObject.isClose)) return
-      }
-  
-      renderCamera()
-    }, false, [isSelected])
+  useMemo(() => {
+    if (!renderTarget.current) {
+      renderTarget.current = new THREE.WebGLRenderTarget(resolution * aspectRatio, resolution)
+    }
+  }, [sceneObject])
 
-    useMemo(() => 
-    {
-      for(let i = 0, n = objectsToRender.length; i < n; i++)
-      {
-        objectsToRender[i].traverse(object  => object.layers.enable(1));
-      }
-    }, [objectsToRender])
+  const mesh = useMemo(() => {
+    if (gltf) {
+      const children = []
+      gltf.scene.traverse(child => {
+        if (child.isMesh) {
+          children.push(
+            <primitive
+              key={sceneObject.id}
+              object={meshFactory(child)}
+            />
+          )
+        }
+      })
+      return children
+    }
+    return []
+  }, [gltf])
 
-    useMemo(()=>
-    {
-      if(virtualCamera.current)
-      {
-        virtualCamera.current.layers.set(1);
-      }
-    }, [virtualCamera.current])
+  const heightShader = useMemo(
+    () => new THREE.MeshBasicMaterial({
+      map: renderTarget.current.texture,
+      side: THREE.FrontSide
+    }),
+    [renderTarget.current]
+  )
 
-    useMemo(() => 
-    {
-      if(!renderTarget.current )
-      {
-        renderTarget.current = new THREE.WebGLRenderTarget(resolution * aspectRatio, resolution);
-      }
-    }, [sceneObject])
+  const cameraView = useMemo(() => {
+    return <group>
+      <mesh
+        userData={{ type: 'view' }}
+        position={[0, 0.3, 0]}
+        material={heightShader}
+      >
+        <planeGeometry attach='geometry' args={[size * aspectRatio, size]} />
+      </mesh>
+    </group>
+  }, true)
 
-    const mesh = useMemo(() => 
-    {
-      if (gltf) {
-        let children = []
-        gltf.scene.traverse(child => {
-          if (child.isMesh) {
-            children.push(
-              <primitive
-                key={sceneObject.id}
-                object={meshFactory(child)}
-              />
-            )
-          }
-        })
-        return children
-      }
-      return []
-    }, [gltf])
+  return <group
+    ref={ref}
+    onController={sceneObject.visible ? () => null : null}
+    userData={{
+      type: 'virtual-camera',
+      id: sceneObject.id
+    }}
+    renderCamera={renderCamera}
+    position={[sceneObject.x, sceneObject.z, sceneObject.y]}
+    rotation={[sceneObject.tilt, sceneObject.rotation, sceneObject.roll]}
 
-
-
-    const heightShader = useMemo(
-        () =>  new THREE.MeshBasicMaterial({
-          map: renderTarget.current.texture,
-          side: THREE.FrontSide,
-        }),
-        [renderTarget.current]
-      )
-
-    const cameraView = useMemo(() =>
-    {
-        return <group>
-            <mesh
-              userData={{ type: 'view' }}
-              position={[0, 0.3, 0]}
-              material={heightShader}
-            >
-              <planeGeometry attach="geometry" args={[size * aspectRatio, size]} />
-            </mesh>
-        </group>
-    }, true);
-    
-
-    return <group
-        ref = {ref}
-        onController={sceneObject.visible ? () => null : null}
-        userData={{
-          type: 'virtual-camera',
-          id: sceneObject.id
-        }}
-        renderCamera = {renderCamera}
-        position={[sceneObject.x, sceneObject.z, sceneObject.y]}
-        rotation={[sceneObject.tilt, sceneObject.rotation, sceneObject.roll]}
-        
-        >   
-        {cameraView}
-        {mesh}
-        <group position = {[0, 0, -0.2]}>
-          <perspectiveCamera
-            name={''}
-            ref={virtualCamera}
-            aspect={aspectRatio}
-            fov={sceneObject.fov}
-            near={0.01}
-            far={1000}
-            onUpdate={self => self.updateProjectionMatrix()}
-          />
-        </group>
-      </group>
+  >
+    {cameraView}
+    {mesh}
+    <group position={[0, 0, -0.2]}>
+      <perspectiveCamera
+        name={''}
+        ref={virtualCamera}
+        aspect={aspectRatio}
+        fov={sceneObject.fov}
+        near={0.01}
+        far={1000}
+        onUpdate={self => self.updateProjectionMatrix()}
+      />
+    </group>
+  </group>
 })
-module.exports = VirtualCamera;
+module.exports = VirtualCamera
