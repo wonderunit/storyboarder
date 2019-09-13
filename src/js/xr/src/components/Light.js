a49ba089c93568c7c8bf602425db2b6a1c3513e8
Eric Skogen <audionerd@users.noreply.github.com>
2019-09-13 15:44:38 -0500

    Light: show in virtual camera

--- src/js/xr/src/components/Light.js
+++ src/js/xr/src/components/Light.js
@@ -1,5 +1,7 @@
 const { useUpdate } = require('react-three-fiber')
 
+const VirtualCamera = require('../components/VirtualCamera')
+
 const Light = React.memo(({ sceneObject, isSelected, texture, children }) => {
   const ref = useUpdate(self => {
     self.rotation.x = 0
@@ -10,12 +12,16 @@ const Light = React.memo(({ sceneObject, isSelected, texture, children }) => {
 
     // TODO
     // spotLight.target.position.set(0, 0, sceneObject.distance)
+
+    self.traverse(child => child.layers.enable(VirtualCamera.VIRTUAL_CAMERA_LAYER))
   }, [sceneObject.rotation, sceneObject.tilt, sceneObject.roll, sceneObject.distance])
 
+  const spotLight = useRef()
   // const spotLight = useUpdate(
   //   self => {
-  //     self.target.position.set(0, 0, sceneObject.distance)
-  //     self.add(self.target)
+  //     // TODO
+  //     // self.target.position.set(0, 0, sceneObject.distance)
+  //     // self.add(self.target)
   //   },
   //   [sceneObject.intensity, sceneObject.distance]
   // )
@@ -31,19 +37,7 @@ const Light = React.memo(({ sceneObject, isSelected, texture, children }) => {
       }}
       position={[sceneObject.x, sceneObject.z, sceneObject.y]}
     >
-      
-      {/* <spotLight
-        ref={spotLight}
-        color={0xffffff}
-        intensity={sceneObject.intensity}
-        position={[0, 0, 0]}
-        rotation={[Math.PI / 2, 0, 0]}
-        angle={sceneObject.angle}
-        distance={sceneObject.distance}
-        penumbra={sceneObject.penumbra}
-        decay={sceneObject.decay}
-      /> */}
-     
+      {/* selectable cone */}
       <mesh>
         <cylinderBufferGeometry attach="geometry" args={[0.0, 0.05, 0.14]} />
         <meshLambertMaterial
@@ -53,24 +47,41 @@ const Light = React.memo(({ sceneObject, isSelected, texture, children }) => {
         />
       </mesh>
 
-      <mesh name="hitter_light" userData={{ type: 'hitter_light' }}>
+      {/* hit target */}
+      <mesh>
         <sphereBufferGeometry attach="geometry" args={[0.125]} />
         <meshLambertMaterial attach="material" visible={false} />
       </mesh>
 
-      <mesh
-        position={[0, (sceneObject.distance + 0.14) * -0.5, 0]}
-        scale={[1, -1, 1]}
-        userData={{ preventInteraction: true }}
-      >
-        <cylinderBufferGeometry
-          attach="geometry"
-          args={[sceneObject.distance * Math.tan(sceneObject.angle), 0.05, sceneObject.distance, 64, 1, true]}
-        />
-        <meshBasicMaterial attach="material" transparent={true} color={0xffffff} opacity={sceneObject.intensity * 0.5}>
-          <primitive attach="map" object={texture} />
-        </meshBasicMaterial>
-      </mesh>
+      <spotLight
+        ref={spotLight}
+        color={0xffffff}
+        intensity={sceneObject.intensity}
+        position={[0, 0, 0]}
+        rotation={[Math.PI / 2, 0, 0]}
+        angle={sceneObject.angle}
+        distance={sceneObject.distance}
+        penumbra={sceneObject.penumbra}
+        decay={sceneObject.decay}
+      />
+
+      {/* simulated light */}
+      {/*
+        <mesh
+          position={[0, (sceneObject.distance + 0.14) * -0.5, 0]}
+          scale={[1, -1, 1]}
+          userData={{ preventInteraction: true }}
+        >
+          <cylinderBufferGeometry
+            attach="geometry"
+            args={[sceneObject.distance * Math.tan(sceneObject.angle), 0.05, sceneObject.distance, 64, 1, true]}
+          />
+          <meshBasicMaterial attach="material" transparent={true} color={0xffffff} opacity={sceneObject.intensity * 0.5}>
+            <primitive attach="map" object={texture} />
+          </meshBasicMaterial>
+        </mesh>
+      */}
+
       {children}
     </group>
   )
