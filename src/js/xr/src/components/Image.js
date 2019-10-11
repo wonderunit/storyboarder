4d8a8894d253f12d7c42fa98004738bd0868b652
Toseben <arekoskela@gmail.com>
2019-10-11 16:32:41 +0200

    XR: plane geometry, no texture

--- src/js/xr/src/components/Image.js
+++ src/js/xr/src/components/Image.js
@@ -1,16 +1,28 @@
+const { useUpdate } = require('react-three-fiber')
+
 const Image = React.memo(({ sceneObject, isSelected }) => {
+  const ref = useUpdate(self => console.log())
+
+  const { x, y, z, visible, width, height, rotation } = sceneObject
 
   return (
-    <group
+    <mesh
+      ref={ref}
+      
       onController={sceneObject.visible ? () => null : null}
-      visible={sceneObject.visible}
       userData={{
         id: sceneObject.id,
         type: 'image'
       }}
-      position={[sceneObject.x, sceneObject.z, sceneObject.y]}
+
+      visible={visible}
+      position={[x, z, y]}
+      scale={[width, height, 1]}
+      rotation={[rotation.x, rotation.y, rotation.z]}
     >
-    </group>
+      <boxBufferGeometry attach='geometry' args={[1, 1, 0.01]} />
+      <meshToonMaterial attach='material' side={THREE.FrontSide}></meshToonMaterial>
+    </mesh>
   )
 })
 
