58f06cbab523725abd0db7001124bfc6d3627e60
Charles Forman <charles@setpixel.com>
2019-10-18 18:19:16 -0400

    updated mac build process

---
+++ scripts/notarize.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const { notarize } = require('electron-notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  console.log({
+    appBundleId: 'com.wonderunit.storyboarder',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLEID,
+    appleIdPassword: process.env.APPLEIDPASS,
+  })
+
+
+  return await notarize({
+    appBundleId: 'com.wonderunit.storyboarder',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLEID,
+    appleIdPassword: process.env.APPLEIDPASS,
+  });
+};
