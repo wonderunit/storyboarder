19231c0d210a2e047dcd3de662b8e1a48910fd80
SergeySharpov <sharpov.peaceit@gmail.com>
2019-12-13 18:30:25 +0200

    code cleanup

--- src/js/windows/shot-generator/main.js
+++ src/js/windows/shot-generator/main.js
@@ -43,23 +43,23 @@ const show = async (onComplete) => {
   }
   
   let { x, y, width, height } = memento
-  
+
   win = new BrowserWindow({
     minWidth: isDev ? undefined : 1200,
     minHeight: isDev ? undefined : 800,
-    
+
     x,
     y,
     width,
     height,
-    
+
     show: false,
     center: true,
     frame: true,
-    
+
     backgroundColor: '#333333',
     titleBarStyle: 'hiddenInset',
-    
+
     acceptFirstMouse: true,
     simpleFullscreen: true,
     webPreferences: {
@@ -71,7 +71,7 @@ const show = async (onComplete) => {
       backgroundThrottling: true,
     }
   })
-  
+
   // via https://github.com/electron/electron/blob/master/docs/api/web-contents.md#event-will-prevent-unload
   //     https://github.com/electron/electron/pull/9331
   //
@@ -83,19 +83,19 @@ const show = async (onComplete) => {
       title: 'Confirm',
       message: 'Your scene is not saved. Are you sure you want to close Shot Generator?'
     })
-    
+
     const leave = (choice === 0)
-    
+
     if (leave) {
       // ignore the default behavior of preventing unload
       // ... which means we'll actually ... _allow_ unload :)
       event.preventDefault()
     }
   })
-  
+
   win.on('resize', () => memento = win.getBounds())
   win.on('move', () => memento = win.getBounds())
-  
+
   win.once('closed', () => {
     win = null
   })
@@ -104,10 +104,10 @@ const show = async (onComplete) => {
     protocol: 'file:',
     slashes: true
   }))
-  
+
   // use this to wait until the window has completely loaded
   // ipcMain.on('shot-generator:window:loaded', () => { })
-  
+
   // use this to show sooner
   win.once('ready-to-show', () => {
     reveal(onComplete)
@@ -138,7 +138,7 @@ ipcMain.on('shot-generator:edit:redo', () => {
 })
 
 ipcMain.on('shot-generator:export-gltf', () =>
-    win.webContents.send('shot-generator:export-gltf'))
+  win.webContents.send('shot-generator:export-gltf'))
 
 module.exports = {
   show,
