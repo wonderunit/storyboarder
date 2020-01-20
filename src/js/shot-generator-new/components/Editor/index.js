dbba39aeb39abb2aba6d964e664c75841905ee13
Serg <sharpov.sergey@theevenstar.net>
2020-01-20 17:57:21 +0200

    Editor fixes

--- src/js/shot-generator-new/components/Editor/index.js
+++ src/js/shot-generator-new/components/Editor/index.js
@@ -92,77 +92,201 @@ import GuidesView from "../GuidesView"
 
 import {gltfLoader} from "../../utils/gltfLoader"
 
-const loadAttachment = ({ filepath, dispatch }) => {
-  switch (path.extname(filepath)) {
-    case '.obj':
-      objLoader.load(
-        filepath,
-        event => {
-          let value = { scene: event.detail.loaderRootNode }
-          log.info('cache: success', filepath)
-          dispatch({ type: 'ATTACHMENTS_SUCCESS', payload: { id: filepath, value } })
-        },
-        null,
-        error => {
-          log.error('cache: error')
-          log.error(error)
-          alert(error)
-          // dispatch({ type: 'ATTACHMENTS_ERROR', payload: { id: filepath, error } })
-          dispatch({ type: 'ATTACHMENTS_DELETE', payload: { id: filepath } })
-        }
-      )
-      return dispatch({ type: 'ATTACHMENTS_LOAD', payload: { id: filepath } })
+const Editor = React.memo(({
+  mainViewCamera, createObject, selectObject, updateModels, loadScene, saveScene, activeCamera, setActiveCamera, resetScene, remoteInput, aspectRatio, sceneObjects, world, selections, selectedBone, onBeforeUnload, setMainViewCamera, withState, attachments, undoGroupStart, undoGroupEnd
+}) => {
+  const smallCanvasRef = useRef(null)
+  const largeCanvasRef = useRef(null)
+  const notificationsRef = useRef(null)
+  const mainViewContainerRef = useRef(null)
 
-    case '.gltf':
-    case '.glb':
-      gltfLoader.load(
-        filepath,
-        value => {
-          log.info('cache: success', filepath)
-          dispatch({ type: 'ATTACHMENTS_SUCCESS', payload: { id: filepath, value } })
-        },
-        null,
-        error => {
-          log.error('cache: error')
-          log.error(error)
-          alert(error)
-          // dispatch({ type: 'ATTACHMENTS_ERROR', payload: { id: filepath, error } })
-          dispatch({ type: 'ATTACHMENTS_DELETE', payload: { id: filepath } })
+  const largeCanvasSize = useComponentSize(mainViewContainerRef)
 
-        }
-      )
-      return dispatch({ type: 'ATTACHMENTS_LOAD', payload: { id: filepath } })
-  }
-}
+  const orthoCamera = useRef(new THREE.OrthographicCamera( -4, 4, 4, -4, 1, 10000 ))
 
-const loadSceneObjects = async (dispatch, state) => {
-  let storyboarderFilePath = state.meta.storyboarderFilePath
-  loadCameraModel(storyboarderFilePath)
-  const loadables = Object.values(getSceneObjects(state))
-  // has a value for model
-  .filter(o => o.model != null)
-  // loaded false or undefined or null
-  .filter(o => o.loaded !== true)
 
-  for (let loadable of loadables) {
-    // don't try to load the box
-    if (loadable.type === 'object' && loadable.model === 'box') {
-      continue
+  const loadAttachment = ({ filepath, dispatch }) => {
+    switch (path.extname(filepath)) {
+      case '.obj':
+        objLoader.load(
+          filepath,
+          event => {
+            let value = { scene: event.detail.loaderRootNode }
+            log.info('cache: success', filepath)
+            dispatch({ type: 'ATTACHMENTS_SUCCESS', payload: { id: filepath, value } })
+          },
+          null,
+          error => {
+            log.error('cache: error')
+            log.error(error)
+            alert(error)
+            // dispatch({ type: 'ATTACHMENTS_ERROR', payload: { id: filepath, error } })
+            dispatch({ type: 'ATTACHMENTS_DELETE', payload: { id: filepath } })
+          }
+        )
+        return dispatch({ type: 'ATTACHMENTS_LOAD', payload: { id: filepath } })
+
+      case '.gltf':
+      case '.glb':
+        gltfLoader.load(
+          filepath,
+          value => {
+            log.info('cache: success', filepath)
+            dispatch({ type: 'ATTACHMENTS_SUCCESS', payload: { id: filepath, value } })
+          },
+          null,
+          error => {
+            log.error('cache: error')
+            log.error(error)
+            alert(error)
+            // dispatch({ type: 'ATTACHMENTS_ERROR', payload: { id: filepath, error } })
+            dispatch({ type: 'ATTACHMENTS_DELETE', payload: { id: filepath } })
+
+          }
+        )
+        return dispatch({ type: 'ATTACHMENTS_LOAD', payload: { id: filepath } })
     }
+  }
 
-    let expectedFilepath = ModelLoader.getFilepathForModel(loadable, { storyboarderFilePath })
+  const loadSceneObjects = async (dispatch, state) => {
+    let storyboarderFilePath = state.meta.storyboarderFilePath
+    loadCameraModel(storyboarderFilePath)
+    const loadables = Object.values(getSceneObjects(state))
+    // has a value for model
+    .filter(o => o.model != null)
+    // loaded false or undefined or null
+    .filter(o => o.loaded !== true)
 
-    // grab the latest state
+    for (let loadable of loadables) {
+      // don't try to load the box
+      if (loadable.type === 'object' && loadable.model === 'box') {
+        continue
+      }
+
+      let expectedFilepath = ModelLoader.getFilepathForModel(loadable, { storyboarderFilePath })
+
+      // grab the latest state
+      withState(async (dispatch, state) => {
+        // if it's in the cache already, skip
+        if (state.attachments[expectedFilepath]) return
+
+        // prevent doubling up
+        dispatch({ type: 'ATTACHMENTS_PENDING', payload: { id: expectedFilepath } })
+
+        // if absolute filepath does not exist ...
+        if (!fs.existsSync(expectedFilepath)) {
+          // ... ask the artist to locate it
+          try {
+
+            const choice = dialog.showMessageBox({
+              type: 'question',
+              buttons: ['Yes', 'No'],
+              title: 'Model file not found',
+              message: `Could not find model file at ${expectedFilepath}. Try to find it?`,
+            })
+
+            const shouldRelocate = (choice === 0)
+
+            if (!shouldRelocate) {
+              throw new Error('could not relocate missing file')
+            }
+
+            let updatedFilepath = await new Promise((resolve, reject) => {
+              dialog.showOpenDialog(
+                {
+                  title: 'Locate model file',
+                  defaultPath: path.dirname(expectedFilepath),
+                  filters: [
+                    {
+                      name: 'Model',
+                      extensions: ['gltf', 'glb']
+                    }
+                  ]
+                },
+                filenames => {
+                  if (filenames) {
+                    resolve(filenames[0])
+                  } else {
+                    reject('no alternate filepath provided')
+                  }
+                }
+              )
+            })
+
+
+            // remove the pending absolute path from attachments
+            dispatch({ type: 'ATTACHMENTS_DELETE', payload: { id: expectedFilepath } })
+            // update ALL instances of the model with the new location
+            dispatch({
+              type: 'ATTACHMENTS_RELOCATE',
+              payload: {
+                src: expectedFilepath,
+                dst: updatedFilepath
+              }
+            })
+            return
+
+          } catch (error) {
+            log.error(error)
+
+            dispatch({ type: 'ATTACHMENTS_DELETE', payload: { id: expectedFilepath } })
+            return
+          }
+        }
+        if (ModelLoader.needsCopy(loadable)) {
+          let src = expectedFilepath
+
+          let dst = path.join(
+            path.dirname(storyboarderFilePath),
+            ModelLoader.projectFolder(loadable.type),
+            path.basename(expectedFilepath)
+          )
+
+          log.info('will copy from', src, 'to', dst)
+
+          // make sure the path exists
+          fs.ensureDirSync(path.dirname(dst))
+
+          // as long as they are different files, we need to copy them
+          if (src !== dst) {
+
+            log.info(`copying model file from ${src} to ${dst}`)
+            fs.copySync(src, dst, { overwrite: true, errorOnExist: false })
+          }
+
+          // update it in the data
+          let updatedModel = path.join(
+            ModelLoader.projectFolder(loadable.type),
+            path.basename(dst)
+          )
+
+          dispatch(updateObject(loadable.id, { model: updatedModel }))
+
+          // remove the pending absolute path from attachments
+          dispatch({ type: 'ATTACHMENTS_DELETE', payload: { id: src } })
+          return
+        }
+
+        loadAttachment({ filepath: expectedFilepath, dispatch })
+      })
+    }
+  }
+
+  const loadWorldEnvironment = async (dispatch, state) => {
+    let storyboarderFilePath = state.meta.storyboarderFilePath
+    const world = getWorld(state)
+
+    let expectedFilepath = ModelLoader.getFilepathForModel({
+      model: world.environment.file,
+      type: 'environment'
+    }, { storyboarderFilePath })
+
     withState(async (dispatch, state) => {
-      // if it's in the cache already, skip
       if (state.attachments[expectedFilepath]) return
 
-      // prevent doubling up
       dispatch({ type: 'ATTACHMENTS_PENDING', payload: { id: expectedFilepath } })
 
-      // if absolute filepath does not exist ...
       if (!fs.existsSync(expectedFilepath)) {
-        // ... ask the artist to locate it
         try {
 
           const choice = dialog.showMessageBox({
@@ -200,25 +324,33 @@ const loadSceneObjects = async (dispatch, state) => {
             )
           })
 
+          log.info('user selected updatedFilepath:', updatedFilepath)
+
+          // TODO test:
+          // handle case where user relocated to a file in the models/* folder
+          //
 
           // remove the pending absolute path from attachments
           dispatch({ type: 'ATTACHMENTS_DELETE', payload: { id: expectedFilepath } })
-          // update ALL instances of the model with the new location
+          // update the instance
           dispatch({
-            type: 'ATTACHMENTS_RELOCATE',
+            type: 'UPDATE_WORLD_ENVIRONMENT',
             payload: {
-              src: expectedFilepath,
-              dst: updatedFilepath
+              file: updatedFilepath
             }
           })
           return
 
         } catch (error) {
           log.error(error)
-
           dispatch({ type: 'ATTACHMENTS_DELETE', payload: { id: expectedFilepath } })
           return
         }
+      }
+
+      let loadable = {
+        model: world.environment.file,
+        type: 'environment'
       }
       if (ModelLoader.needsCopy(loadable)) {
         let src = expectedFilepath
@@ -231,25 +363,25 @@ const loadSceneObjects = async (dispatch, state) => {
 
         log.info('will copy from', src, 'to', dst)
 
-        // make sure the path exists
         fs.ensureDirSync(path.dirname(dst))
 
-        // as long as they are different files, we need to copy them
         if (src !== dst) {
-
           log.info(`copying model file from ${src} to ${dst}`)
           fs.copySync(src, dst, { overwrite: true, errorOnExist: false })
         }
 
-        // update it in the data
         let updatedModel = path.join(
           ModelLoader.projectFolder(loadable.type),
           path.basename(dst)
         )
 
-        dispatch(updateObject(loadable.id, { model: updatedModel }))
-
-        // remove the pending absolute path from attachments
+        log.info('copied! updated model:', updatedModel)
+        dispatch({
+          type: 'UPDATE_WORLD_ENVIRONMENT',
+          payload: {
+            file: updatedModel
+          }
+        })
         dispatch({ type: 'ATTACHMENTS_DELETE', payload: { id: src } })
         return
       }
@@ -257,136 +389,6 @@ const loadSceneObjects = async (dispatch, state) => {
       loadAttachment({ filepath: expectedFilepath, dispatch })
     })
   }
-}
-
-const loadWorldEnvironment = async (dispatch, state) => {
-  let storyboarderFilePath = state.meta.storyboarderFilePath
-
-  let expectedFilepath = ModelLoader.getFilepathForModel({
-    model: world.environment.file,
-    type: 'environment'
-  }, { storyboarderFilePath })
-
-  withState(async (dispatch, state) => {
-    if (state.attachments[expectedFilepath]) return
-
-    dispatch({ type: 'ATTACHMENTS_PENDING', payload: { id: expectedFilepath } })
-
-    if (!fs.existsSync(expectedFilepath)) {
-      try {
-
-        const choice = dialog.showMessageBox({
-          type: 'question',
-          buttons: ['Yes', 'No'],
-          title: 'Model file not found',
-          message: `Could not find model file at ${expectedFilepath}. Try to find it?`,
-        })
-
-        const shouldRelocate = (choice === 0)
-
-        if (!shouldRelocate) {
-          throw new Error('could not relocate missing file')
-        }
-
-        let updatedFilepath = await new Promise((resolve, reject) => {
-          dialog.showOpenDialog(
-            {
-              title: 'Locate model file',
-              defaultPath: path.dirname(expectedFilepath),
-              filters: [
-                {
-                  name: 'Model',
-                  extensions: ['gltf', 'glb']
-                }
-              ]
-            },
-            filenames => {
-              if (filenames) {
-                resolve(filenames[0])
-              } else {
-                reject('no alternate filepath provided')
-              }
-            }
-          )
-        })
-
-        log.info('user selected updatedFilepath:', updatedFilepath)
-
-        // TODO test:
-        // handle case where user relocated to a file in the models/* folder
-        //
-
-        // remove the pending absolute path from attachments
-        dispatch({ type: 'ATTACHMENTS_DELETE', payload: { id: expectedFilepath } })
-        // update the instance
-        dispatch({
-          type: 'UPDATE_WORLD_ENVIRONMENT',
-          payload: {
-            file: updatedFilepath
-          }
-        })
-        return
-
-      } catch (error) {
-        log.error(error)
-        dispatch({ type: 'ATTACHMENTS_DELETE', payload: { id: expectedFilepath } })
-        return
-      }
-    }
-
-    let loadable = {
-      model: world.environment.file,
-      type: 'environment'
-    }
-    if (ModelLoader.needsCopy(loadable)) {
-      let src = expectedFilepath
-
-      let dst = path.join(
-        path.dirname(storyboarderFilePath),
-        ModelLoader.projectFolder(loadable.type),
-        path.basename(expectedFilepath)
-      )
-
-      log.info('will copy from', src, 'to', dst)
-
-      fs.ensureDirSync(path.dirname(dst))
-
-      if (src !== dst) {
-        log.info(`copying model file from ${src} to ${dst}`)
-        fs.copySync(src, dst, { overwrite: true, errorOnExist: false })
-      }
-
-      let updatedModel = path.join(
-        ModelLoader.projectFolder(loadable.type),
-        path.basename(dst)
-      )
-
-      log.info('copied! updated model:', updatedModel)
-      dispatch({
-        type: 'UPDATE_WORLD_ENVIRONMENT',
-        payload: {
-          file: updatedModel
-        }
-      })
-      dispatch({ type: 'ATTACHMENTS_DELETE', payload: { id: src } })
-      return
-    }
-
-    loadAttachment({ filepath: expectedFilepath, dispatch })
-  })
-}
-
-const Editor = React.memo(({
-  mainViewCamera, createObject, selectObject, updateModels, loadScene, saveScene, activeCamera, setActiveCamera, resetScene, remoteInput, aspectRatio, sceneObjects, world, selections, selectedBone, onBeforeUnload, setMainViewCamera, withState, attachments, undoGroupStart, undoGroupEnd
-}) => {
-  const smallCanvasRef = useRef(null)
-  const largeCanvasRef = useRef(null)
-  const notificationsRef = useRef(null)
-  const mainViewContainerRef = useRef(null)
-
-  const largeCanvasSize = useComponentSize(mainViewContainerRef)
-
-  const orthoCamera = useRef(new THREE.OrthographicCamera( -4, 4, 4, -4, 1, 10000 ))
 
   useEffect(() => {
     if (notificationsRef.current) {
@@ -546,6 +548,32 @@ export default connect(
   }),
   {
     withState,
-    setMainViewCamera
+    setMainViewCamera,
+
+    createObject,
+    selectObject,
+    updateModels: payload => ({ type: 'UPDATE_MODELS', payload }),
+    loadScene,
+    saveScene: filepath => (dispatch, getState) => {
+      let state = getState()
+      let contents = getSerializedState(state)
+      fs.writeFileSync(filepath, JSON.stringify(contents, null, 2))
+      dialog.showMessageBox(null, { message: 'Saved!' })
+      // dispatch(markSaved())
+    },
+    setActiveCamera,
+    resetScene,
+
+    onBeforeUnload: event => (dispatch, getState) => {
+      if (getIsSceneDirty(getState())) {
+        // pass electron-specific flag
+        // to trigger `will-prevent-unload` on BrowserWindow
+        event.returnValue = false
+      }
+    },
+    markSaved,
+
+    undoGroupStart,
+    undoGroupEnd
   }
 )(Editor)
