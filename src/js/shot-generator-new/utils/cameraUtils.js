35a2e057a0828e20f177e114087b6b400fe2e36e
Vlad Stepura <vlad.stepura3d@gmail.com>
2020-02-04 10:44:54 +0200

    Fixed creating second camera throws an error

--- src/js/shot-generator-new/utils/cameraUtils.js
+++ src/js/shot-generator-new/utils/cameraUtils.js
@@ -232,7 +232,8 @@ const getShotBox = (character, shotType = 0) => {
   
   /** If shot isn't provided, use eyes to calculate shot angle */
   if (!ShotSizesInfo[shotType]) {
-    let bone = character.userData.skeleton.bones.find((bone) => bone.name === 'leaf')
+    let skinnedMesh = character.getObjectByProperty("type", "SkinnedMesh")
+    let bone = skinnedMesh.skeleton.bones.find((bone) => bone.name === 'leaf')
     
     let boneInfo = getBoneStartEndPos(bone)
     box.expandByPoint(boneInfo.start)
