93518a163e7ae99b5c596097cc56b85e700f701e
Vlad Stepura <vlad.stepura3d@gmail.com>
2020-04-10 14:22:28 +0300

    Added long shots to area rule exceptions

--- src/js/shot-explorer/ShotsRule/AreaShotRule.js
+++ src/js/shot-explorer/ShotsRule/AreaShotRule.js
@@ -1,68 +1,65 @@
 import ShotRule from "./ShotRule"
-import { ShotSizes, ShotAngles, setShot, getClosestCharacter } from '../../shot-generator/utils/cameraUtils'
 import * as THREE from 'three'
+
+const isBoneInShot = (bone) => {
+    let name = bone.name;
+    return name === "Neck" || name === "Head" || name === "leaf" 
+            || name === "LeftEye" || name === "RightEye" || name === "LeftShoulder"
+            || name === "RightShoulder";
+}
+
 class AreaShotRule extends ShotRule {
     constructor(focusedCenter, camera, characters, shot) {
         super(focusedCenter, camera);
-        this.characters = characters;//.filter(object => object.uuid !== shot.character.uuid);
+        this.characters = characters;
         this.shot = shot;
         this.radius = 1.5;
     }
 
     applyRule(scene) {
         super.applyRule();
-        let character = this.shot.character
-        let charactersInRange = []
-        let characterPosition = character.worldPosition()
-        let headPoints = []
+        let character = this.shot.character;
+        let charactersInRange = [];
+        let characterPosition = character.worldPosition();
+        let headPoints = [];
         for( let i = 0; i < this.characters.length; i++) {
-            let position = this.characters[i].worldPosition()
+            let position = this.characters[i].worldPosition();
             if(Math.pow(position.x - characterPosition.x, 2) + Math.pow(position.y - characterPosition.y, 2) < Math.pow(this.radius, 2)) {
-                charactersInRange.push(this.characters[i])
-                let headBone = this.characters[i].getObjectByProperty("type", "SkinnedMesh").skeleton.bones.filter(bone => bone.name === "Neck")[0]
-                
-                headPoints.push(headBone.worldPosition())
-                for(let i = 0; i < headBone.children.length; i++) {
-                    //if(headBone.children[i].name.includes('leaf'))
-                    headPoints.push(headBone.children[i].worldPosition())
-                }
-                
+                let shotCharacter = this.characters[i];
+                charactersInRange.push(shotCharacter);
+                headPoints = headPoints.concat(this.getCharacterShotPoints(shotCharacter));
             }
         }
-        let headBone = character.getObjectByProperty("type", "SkinnedMesh").skeleton.bones.filter(bone => bone.name === "Head")[0]
-        headPoints.push(headBone.worldPosition())
-        for(let i = 0; i < headBone.children.length; i++) {
-            if(headBone.children[i].name.includes('leaf'))
-            headPoints.push(headBone.children[i].worldPosition())
-        }
-        let box = new THREE.Box3().setFromPoints(headPoints)
-        console.log(charactersInRange)
+        let box = new THREE.Box3().setFromPoints(headPoints);
         if(charactersInRange.length > 1) {
 
             //#region Camera distancing method
             let center = this.focusedCenter;
-            let areaCenter = new THREE.Vector3()
+            let areaCenter = new THREE.Vector3();
             box.getCenter(areaCenter)
             areaCenter.y = box.max.y
             areaCenter.x = center.x
             areaCenter.z = center.z
-            let BA = new THREE.Vector3().subVectors(center, this.camera.position)
-            let BC = new THREE.Vector3().subVectors(areaCenter, this.camera.position)
+            let BA = new THREE.Vector3().subVectors(center, this.camera.position);
+            let BC = new THREE.Vector3().subVectors(areaCenter, this.camera.position);
             let cosineAngle = BA.dot(BC) / (BA.length() * BC.length());
             let angle = Math.acos(cosineAngle);
 
-            let difference = center.clone().sub(areaCenter)
-            let normalCenter = difference.clone().normalize()
-            normalCenter.set(normalCenter.y, normalCenter.x, 0)
-            let sphere = new THREE.Sphere()
-            box.getBoundingSphere(sphere)
-            this.camera.rotateOnAxis(normalCenter, angle)
-            let direction = new THREE.Vector3()
-            this.camera.getWorldDirection(direction)
-            direction.negate()
-            let h = sphere.radius / Math.tan(this.camera.fov / 2 * Math.PI / 180.0)
-            let newPos = new THREE.Vector3().addVectors(sphere.center, direction.clone().setLength(h))
-            this.camera.position.copy(newPos)
+            let difference = center.clone().sub(areaCenter);
+            let normalCenter = difference.clone().normalize();
+            normalCenter.set(normalCenter.y, normalCenter.x, 0);
+            let sphere = new THREE.Sphere();
+            box.getBoundingSphere(sphere);
+            this.camera.rotateOnAxis(normalCenter, angle);
+            let direction = new THREE.Vector3();
+            this.camera.getWorldDirection(direction);
+            direction.negate();
+            let depth = sphere.radius / Math.tan(this.camera.fov / 2 * Math.PI / 180.0);
+            let newPos = new THREE.Vector3().addVectors(sphere.center, direction.clone().setLength(depth));
+            console.log(sphere.center.distanceTo(this.camera.position), sphere.center.distanceTo(newPos) + this.radius)
+            if(sphere.center.distanceTo(newPos) + this.radius > sphere.center.distanceTo(this.camera.position)) {
+                this.camera.position.copy(newPos)
+            }
             this.camera.updateMatrixWorld(true);
             //#endregion
 
@@ -115,6 +112,18 @@ class AreaShotRule extends ShotRule {
         }
         return null
     }
+
+
+    //#region private methods
+    getCharacterShotPoints(character) {
+        let headPoints = [];
+        let shotBones = character.getObjectByProperty("type", "SkinnedMesh").skeleton.bones.filter(bone => isBoneInShot(bone));
+        for(let i = 0; i < shotBones.length; i++) {
+            headPoints.push(shotBones[i].worldPosition());
+        }
+        return headPoints;
+    }
+    //#endregion
 }
 
 export default AreaShotRule;
