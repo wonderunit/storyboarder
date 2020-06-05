27e872912d421c064c33f17c63a75731890bf060
Vlad Stepura <vlad.stepura3d@gmail.com>
2020-06-05 12:43:19 +0300

    Added three standard pattern recognition

--- src/js/shot-generator/components/Three/SceneBackground.js
+++ src/js/shot-generator/components/Three/SceneBackground.js
@@ -24,16 +24,49 @@ const getCubeMapTextures = (gltf, boardPath) => {
     drawingCanvas.height = image.height;
     fs.removeSync(path.join(path.dirname(boardPath), 'models/sceneTextures/cubetexture'))
 
+    // 4 by 3 pattern when cubetexture has 4 columns and 3 rows 
+    // -  py -  -
+    // nx pz px nz
+    // -  ny -  -
     if(image.width / 4 === image.height / 3) {
         let elementSize = image.width / 4  
         cropedCanvas.width = elementSize;
         cropedCanvas.height = elementSize;
-        crop(image, elementSize, 0, elementSize, elementSize, "py", boardPath)
-        crop(image, 0, elementSize, elementSize, elementSize, "nx", boardPath)
-        crop(image, elementSize, elementSize, elementSize, elementSize, "pz", boardPath)
-        crop(image, elementSize * 2, elementSize, elementSize, elementSize, "px", boardPath)
-        crop(image, elementSize * 3, elementSize, elementSize, elementSize, "nz", boardPath)
-        crop(image, elementSize, elementSize * 2, elementSize, elementSize, "ny", boardPath)
+        crop(image, elementSize,        0,                  elementSize, elementSize, "py", boardPath)
+        crop(image, 0,                  elementSize,        elementSize, elementSize, "nx", boardPath)
+        crop(image, elementSize,        elementSize,        elementSize, elementSize, "pz", boardPath)
+        crop(image, elementSize * 2,    elementSize,        elementSize, elementSize, "px", boardPath)
+        crop(image, elementSize * 3,    elementSize,        elementSize, elementSize, "nz", boardPath)
+        crop(image, elementSize,        elementSize * 2,    elementSize, elementSize, "ny", boardPath)
+    }
+    // 3 by 4 pattern when cubetexture has 3 columns and 4 rows
+    // -  py -
+    // pz px nz
+    // -  ny -
+    // -  nx -
+    if(image.width / 3 === image.height / 4) {
+        let elementSize = image.width / 3  
+        cropedCanvas.width = elementSize
+        cropedCanvas.height = elementSize
+        crop(image, elementSize,     0,               elementSize, elementSize, "py", boardPath)
+        crop(image, 0,               elementSize,     elementSize, elementSize, "pz", boardPath)
+        crop(image, elementSize,     elementSize,     elementSize, elementSize, "px", boardPath)
+        crop(image, elementSize * 2, elementSize,     elementSize, elementSize, "nz", boardPath)
+        crop(image, elementSize,     elementSize * 2, elementSize, elementSize, "ny", boardPath)
+        crop(image, elementSize,     elementSize * 3, elementSize, elementSize, "nx", boardPath)
+    }
+
+    let row = image.width / 6 === image.height 
+    let column = image.width === image.height / 6
+    // 1 by 6 pattern when either we have 1 column or 1 row
+    // px nx py ny pz nz
+    if(row || column) {
+        let elementSize = row ? image.height : image.width
+        cropedCanvas.width = elementSize;
+        cropedCanvas.height = elementSize;
+        for( let i = 0; i < faces.length; i++ ) {
+            crop(image, elementSize * row * i, elementSize * i * column, elementSize, elementSize, faces[i], boardPath)
+        }
     }
 
     return new THREE.CubeTextureLoader()
