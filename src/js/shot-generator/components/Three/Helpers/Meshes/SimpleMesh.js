f93d6e4622893ccc078fc55dd247df80edc5cb41
Vlad Stepura <vlad.stepura3d@gmail.com>
2020-06-18 16:32:59 +0300

    Improved simple brush

--- src/js/shot-generator/components/Three/Helpers/Meshes/SimpleMesh.js
+++ src/js/shot-generator/components/Three/Helpers/Meshes/SimpleMesh.js
@@ -11,12 +11,22 @@ class SimpleMesh extends Mesh {
         this.drawingCtx.strokeStyle = mesh.color;
         this.drawingCtx.lineWidth = mesh.size;
       
+
+        var circle = new Path2D();
+        circle.moveTo(this.prevPos.x, this.prevPos.y );
+        circle.arc(this.prevPos.x, this.prevPos.y, mesh.size / 2.1, 0, 2 * Math.PI);
+        this.drawingCtx.fill(circle);
         this.drawingCtx.beginPath();
         this.drawingCtx.moveTo(this.prevPos.x, this.prevPos.y);
         this.drawingCtx.lineTo(currentPos.x, currentPos.y);
 
         this.drawingCtx.stroke();
-        //this.drawingCtx.closePath();
+        this.drawingCtx.closePath();
+
+        circle.moveTo(currentPos.x, currentPos.y );
+        circle.arc(currentPos.x, currentPos.y, mesh.size / 2.1, 0, 2 * Math.PI);
+        this.drawingCtx.fill(circle);
+
         this.prevPos.x = currentPos.x;
         this.prevPos.y = currentPos.y;
     }
