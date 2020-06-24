9f166e552dd29a9ae9025482aefadbe5003ae9d9
Vlad Stepura <vlad.stepura3d@gmail.com>
2020-06-24 14:14:22 +0300

    Fixed eraser brush

--- src/js/shot-generator/components/Three/Helpers/Brushes/EraserBrush.js
+++ src/js/shot-generator/components/Three/Helpers/Brushes/EraserBrush.js
@@ -7,10 +7,19 @@ class EraserBrush extends Brush {
     draw(currentPos, brush) {
         super.draw(currentPos, brush);
         this.drawingCtx.fillStyle = 'white';
-
+        let prevX 
+        let prevY 
+        if(this.positionBuffer.currentLength === 0) {
+            prevX = currentPos.x;
+            prevY = currentPos.y;
+        } else {
+            let prevElements = this.positionBuffer.getElements(this.positionBuffer.currentLength - 1);
+            prevX = prevElements[0];
+            prevY = prevElements[1];
+        }
         let circle = new Path2D();
-        let xOffset = currentPos.x - this.prevPos.x;
-        let yOffset = currentPos.y - this.prevPos.y;
+        let xOffset = currentPos.x - prevX;
+        let yOffset = currentPos.y - prevY;
         let length
         if(Math.abs(xOffset) < Math.abs(yOffset)) {
             length = Math.abs(yOffset)
@@ -24,13 +33,12 @@ class EraserBrush extends Brush {
         for(let i = 0; i < length; i++) {
             let x = xOffset * i;
             let y = yOffset * i;
-            circle.moveTo(this.prevPos.x + x, this.prevPos.y + y);
-            circle.arc(this.prevPos.x + x, this.prevPos.y + y, size, 0, 2 * Math.PI)    
+            circle.moveTo(prevX + x, prevY + y);
+            circle.arc(prevX + x, prevY + y, size, 0, 2 * Math.PI)    
         }
         this.drawingCtx.stroke();
         this.drawingCtx.fill(circle)
-        this.prevPos.x = currentPos.x;
-        this.prevPos.y = currentPos.y;
+        this.positionBuffer.addElements(currentPos.x, currentPos.y);
     }
 }
 
