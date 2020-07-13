41ebf89bc2cd8e046067ceee77e06f5d3f13fbdf
Vlad Stepura <vlad.stepura3d@gmail.com>
2020-07-13 17:52:05 +0300

    Added grayscale to xr and sshot-explorer

--- src/js/windows/shot-generator/window.js
+++ src/js/windows/shot-generator/window.js
@@ -28,12 +28,7 @@ let sendedAction = null
 
 const {SGMiddleware} = require('./../../xr/sockets')
 
-/// Monkey path three js to have grayscale effect
-const THREE = require('three')
-const {fragmentShaderChunk } = require("../../shot-generator/helpers/GrayscaleFragmentShader")
-let toonFragmentShader = THREE.ShaderLib.toon.fragmentShader
-THREE.ShaderLib.toon.fragmentShader = toonFragmentShader.replace(/\}\s*$/, fragmentShaderChunk + '\n}' )
-///
+require("../../shared/helpers/monkeyPatchGrayscale")
 
 const shotExplorerMiddleware = store => next => action => {
   if(!action) return
