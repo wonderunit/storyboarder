7839fb6f41d0968735c98417619f8118fa8bf15e
Eric Skogen <audionerd@users.noreply.github.com>
2020-09-28 13:01:05 -0500

    SearchList: prettier

--- src/js/shot-generator/components/SearchList/index.js
+++ src/js/shot-generator/components/SearchList/index.js
@@ -1,32 +1,38 @@
-import React, {  useCallback} from 'react'
+import React, { useCallback } from 'react'
 import LiquidMetal from 'liquidmetal'
 
-const SearchList = React.memo(({ list, onSearch, label = "Search models …"}) => {
-
-    const onSearchChange = useCallback((event) => {
+const SearchList = React.memo(
+  ({ list, onSearch, label = 'Search models …' }) => {
+    const onSearchChange = useCallback(
+      (event) => {
         event.stopPropagation()
         let terms = event.currentTarget.value
         const matchAll = terms == null || terms.length === 0
-        let filteredList = list.filter(model => {
-            if(matchAll) return true
-            let values = model.value.split("|")
-            for(let i = 0; i < values.length; i++) {
-             if(LiquidMetal.score(values[i], terms) > 0.8) return true
-            }
-            return false
-            })
+        let filteredList = list.filter((model) => {
+          if (matchAll) return true
+          let values = model.value.split('|')
+          for (let i = 0; i < values.length; i++) {
+            if (LiquidMetal.score(values[i], terms) > 0.8) return true
+          }
+          return false
+        })
         onSearch(filteredList)
-    }, [list])
+      },
+      [list]
+    )
 
     const keyDown = useCallback((event) => event.stopPropagation(), [])
 
-    return <div className="column" style={{ flex: 1 }}> 
-            <input
-              placeholder={ label }
-              onChange={ onSearchChange } 
-              onKeyDown={ keyDown }>
-            </input>
-        </div>
-})
+    return (
+      <div className="column" style={{ flex: 1 }}>
+        <input
+          placeholder={label}
+          onChange={onSearchChange}
+          onKeyDown={keyDown}
+        ></input>
+      </div>
+    )
+  }
+)
 
 export default SearchList
