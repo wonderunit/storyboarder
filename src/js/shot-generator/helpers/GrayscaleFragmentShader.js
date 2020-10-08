025e8bec5d7472f567590652fd1f468f350c9665
Vlad Stepura <vlad.stepura3d@gmail.com>
2020-10-08 14:34:06 +0300

    Fixed flatshaded environmens are black

--- src/js/shot-generator/helpers/GrayscaleFragmentShader.js
+++ src/js/shot-generator/helpers/GrayscaleFragmentShader.js
@@ -2,9 +2,9 @@
 
 export const fragmentShaderChunk = [
     "  #ifdef GRAYSCALE",
-    "   float gray = dot(gl_FragColor.rgb, vec3(0.2627, 0.678, 0.0593));",
+    "   float gray = dot(gl_FragColor.rgb, vec3(0.3126, 0.8152, 0.1722));",
     "   vec3 grayscale = vec3(gray);",
-    "	gl_FragColor = vec4(grayscale, gl_FragColor.a );",
+    "	gl_FragColor = vec4(grayscale, gl_FragColor.a);",
     "  #endif",
 ].join("\n")
 
