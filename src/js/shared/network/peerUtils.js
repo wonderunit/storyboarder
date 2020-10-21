344ee7c0a9512eb6f0c41cf5a37076c4804fed88
Eric Skogen <audionerd@users.noreply.github.com>
2020-10-21 12:20:17 -0500

    filepath

--- src/js/shared/network/peerUtils.js
+++ src/js/shared/network/peerUtils.js
@@ -4,9 +4,9 @@ import {promisify} from 'util'
 const readFile = promisify(fs.readFile)
 
 // Loads file content as ArrayBuffer for sending through peerjs
-export const loadFileToBlob = async (path) => {
+export const loadFileToBlob = async (filepath) => {
   try {
-    const contents = await readFile(path)
+    const contents = await readFile(filepath)
     return Uint8Array.from(contents).buffer
   } catch (err) {
     console.error(err)
