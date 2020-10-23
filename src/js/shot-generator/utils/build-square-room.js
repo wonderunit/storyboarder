207a314dc553ce3c28ce41b5a2cee4d239061c3a
Eric Skogen <audionerd@users.noreply.github.com>
2020-10-23 15:17:02 -0500

    build-square-room: remove unused BoundingUVGenerator
    fixes warning: "export 'UV' (imported as 'THREE') was not found in 'three'
    
    ExtrudeGeometry was ignoring the `uvGenerator` setting (it only accepts UVGenerator, case-sensitive)
    UV was replaced with Vector2 in THREE r54

--- src/js/shot-generator/utils/build-square-room.js
+++ src/js/shot-generator/utils/build-square-room.js
@@ -1,73 +1,5 @@
 import * as THREE from 'three'
 
-const BoundingUVGenerator = {
-  generateTopUV: function (geometry, extrudedShape, extrudeOptions, indexA, indexB, indexC) {
-    var ax = geometry.vertices[ indexA ].x,
-      ay = geometry.vertices[ indexA ].y,
-
-      bx = geometry.vertices[ indexB ].x,
-      by = geometry.vertices[ indexB ].y,
-
-      cx = geometry.vertices[ indexC ].x,
-      cy = geometry.vertices[ indexC ].y,
-
-      bb = extrudedShape.getBoundingBox(),
-      bbx = bb.maxX - bb.minX,
-      bby = bb.maxY - bb.minY
-
-    return [
-      new THREE.UV((ax - bb.minX) / bbx, 1 - (ay - bb.minY) / bby),
-      new THREE.UV((bx - bb.minX) / bbx, 1 - (by - bb.minY) / bby),
-      new THREE.UV((cx - bb.minX) / bbx, 1 - (cy - bb.minY) / bby)
-    ]
-  },
-
-  generateBottomUV: function (geometry, extrudedShape, extrudeOptions, indexA, indexB, indexC) {
-    return this.generateTopUV(geometry, extrudedShape, extrudeOptions, indexA, indexB, indexC)
-  },
-
-  generateSideWallUV: function (geometry, extrudedShape, wallContour, extrudeOptions,
-                                  indexA, indexB, indexC, indexD, stepIndex, stepsLength,
-                                  contourIndex1, contourIndex2) {
-    var ax = geometry.vertices[ indexA ].x,
-      ay = geometry.vertices[ indexA ].y,
-      az = geometry.vertices[ indexA ].z,
-
-      bx = geometry.vertices[ indexB ].x,
-      by = geometry.vertices[ indexB ].y,
-      bz = geometry.vertices[ indexB ].z,
-
-      cx = geometry.vertices[ indexC ].x,
-      cy = geometry.vertices[ indexC ].y,
-      cz = geometry.vertices[ indexC ].z,
-
-      dx = geometry.vertices[ indexD ].x,
-      dy = geometry.vertices[ indexD ].y,
-      dz = geometry.vertices[ indexD ].z
-
-    var amt = extrudeOptions.depth,
-      bb = extrudedShape.getBoundingBox(),
-      bbx = bb.maxX - bb.minX,
-      bby = bb.maxY - bb.minY
-
-    if (Math.abs(ay - by) < 0.01) {
-      return [
-        new THREE.UV(ax / bbx, az / amt),
-        new THREE.UV(bx / bbx, bz / amt),
-        new THREE.UV(cx / bbx, cz / amt),
-        new THREE.UV(dx / bbx, dz / amt)
-      ]
-    } else {
-      return [
-        new THREE.UV(ay / bby, az / amt),
-        new THREE.UV(by / bby, bz / amt),
-        new THREE.UV(cy / bby, cz / amt),
-        new THREE.UV(dy / bby, dz / amt)
-      ]
-    }
-  }
-}
-
 const buildSquareRoom = (w, l, h, { textures }) => {
   var hw = w / 2
   var hl = l / 2
@@ -83,8 +15,7 @@ const buildSquareRoom = (w, l, h, { textures }) => {
   var extrudeSettings = {
     steps: 1,
     depth: h / scale,
-    bevelEnabled: false,
-    uvGenerator: BoundingUVGenerator
+    bevelEnabled: false
   }
 
   var materialWall = new THREE.MeshToonMaterial({map: textures.wall, side: THREE.FrontSide})
