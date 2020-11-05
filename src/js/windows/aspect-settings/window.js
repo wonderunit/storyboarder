3aa51c86d2ce1811d9402858f6d69cbfb6e0d176
Vlad Stepura <vlad.stepura3d@gmail.com>
2020-11-05 14:35:12 +0200

    Fixed aspect settings localization

--- src/js/windows/aspect-settings/window.js
+++ src/js/windows/aspect-settings/window.js
@@ -1,8 +1,25 @@
 const { ipcRenderer } = require('electron')
+const LocalizationService = require('../../utils/LocalizationService')
+translateHtml = (elementName, translation) => {
+  let elem = document.querySelector(elementName)
+  if(!elem) return
+  let array =  translation.split("\n")
+  elem.innerHTML = array.map(text => `${text}<br/>`).join("")
+}
 
-ipcRenderer.on("languageChanged", (event, lng) => {
-  i18n.changeLanguage(lng)
-})
+const updateHTMLText = (i18n) => {
+  translateHtml("#aspect-title", i18n.t("new-window.aspect-title"))
+  translateHtml("#aspect-ultrawide", i18n.t("new-window.aspect-ultrawide"))
+  translateHtml("#aspect-doublewide", i18n.t("new-window.aspect-doublewide"))
+  translateHtml("#aspect-wide", i18n.t("new-window.aspect-wide"))
+  translateHtml("#aspect-hd", i18n.t("new-window.aspect-hd"))
+  translateHtml("#aspect-vertical-hd", i18n.t("new-window.aspect-vertical-hd"))
+  translateHtml("#aspect-square", i18n.t("new-window.aspect-square"))
+  translateHtml("#aspect-old", i18n.t("new-window.aspect-old"))
+  translateHtml("#aspect-description", i18n.t("new-window.aspect-description"))
+}
+
+let localizationService = new LocalizationService(updateHTMLText)
 
 document.querySelectorAll('.example').forEach(el => {
     el.addEventListener('click', event => {
