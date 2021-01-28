92b866c736f52a1b7f72e344604bcd681323c25a
Eric Skogen <audionerd@users.noreply.github.com>
2021-01-28 15:33:17 -0600

    remove unused toPdfJs

--- src/js/exporters/pdf/to-pdf-js.js
+++
@@ -1,8 +0,0 @@
-const getStream = require('get-stream')
-
-const toPdfJs = async doc => {
-  let result = await getStream.buffer(doc)
-  return atob(Buffer.from(result).toString('base64'))
-}
-
-module.exports = toPdfJs
