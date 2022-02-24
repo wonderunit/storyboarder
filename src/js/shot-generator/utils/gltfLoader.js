27228013ab7a42b968d6a9b05b26066c7e954d80
Victor <victor.b@theevenstar.net>
2022-02-24 17:08:09 +0200

    add DRACOLoader

--- src/js/shot-generator/utils/gltfLoader.js
+++ src/js/shot-generator/utils/gltfLoader.js
@@ -1,9 +1,13 @@
 import * as THREE from 'three'
 
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
 
 const loadingManager = new THREE.LoadingManager()
 const gltfLoader = new GLTFLoader(loadingManager)
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('./js/vendor/draco/')
+gltfLoader.setDRACOLoader( dracoLoader )
 //THREE.Cache.enabled = true
 
 export { gltfLoader }
